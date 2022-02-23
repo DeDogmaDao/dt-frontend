d3e8ce8a299b7c124caad9a860ff7a67989f224a
aly3n <aly3n.dev@gmail.com>
2022-02-23 21:00:37 +0330

    h styles/font/new colors added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Fragment } from "react";
 // libs
 import { config as fontawesomeConfig } from "@fortawesome/fontawesome-svg-core";
 // components
-import Main from "../components/layout/Main"
+import Main from "../components/layout/Main";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
 // hooks
@@ -11,20 +11,24 @@ import Footer from "../components/layout/Footer";
 // utils & animation
 import "../../styles/tailwind.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
+import Head from "next/head";
 
 fontawesomeConfig.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
-
   return (
-    <Fragment>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet" />
+      </Head>
       <Header />
       <Main>
-      <Component {...pageProps} />
+        <Component {...pageProps} />
       </Main>
       <Footer />
-    </Fragment>
+    </>
   );
 }
 
