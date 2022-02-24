580f7db260bf7702a657b1bface52c35da450008
aly3n <aly3n.dev@gmail.com>
2022-02-24 23:35:37 +0330

    discord btn style change

--- src/components/global/Buttons/DiscordBtn.tsx
+++ src/components/global/Buttons/DiscordBtn.tsx
@@ -1,4 +1,4 @@
-import {  faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { AnimationControls, motion, useAnimation } from "framer-motion";
 import { discordBtnAni } from "../../../utils/animation";
@@ -8,19 +8,19 @@ const DiscordBtn: React.FC = () => {
     <motion.button
       onHoverStart={() => controls.start("visible")}
       onHoverEnd={() => controls.start("hidden")}
-      className="relative w-[180px] h-[60px] flex justify-center items-center gap-x-2 bg-primary-500 rounded-2xl text-blackPrime font-bold"
+      className="relative w-[180px] h-[40px] flex justify-center items-center gap-x-5 bg-primary-500 hover:bg-opacity-90 hover:scale-110 duration-500 text-blackPrime font-bold text-xl"
     >
       <motion.svg
         width="180px"
-        height="60px"
-        viewBox="0 0 180 60"
+        height="40px"
+        viewBox="0 0 180 40"
         className="absolute top-0 left-0 z-0"
       >
         <motion.polyline
           initial="hidden"
           animate={controls}
           variants={discordBtnAni}
-          points="179,1 179,59 1,59 1,1 179,1"
+          points="179,1 179,39 1,39 1,1 179,1"
           strokeWidth={5}
           stroke={"#fff"}
           fill="none"
--- src/components/howItWorksPage/HeaderImg/index.tsx
+++ src/components/howItWorksPage/HeaderImg/index.tsx
@@ -6,7 +6,7 @@ const HeaderImg: React.FC = () => {
       <img className="" src="/media/bg/howitworksbg.jpg" />
       <div className="bg-black/30 w-full h-full absolute left-0 top-0" />
       <h2 className="absolute top-56 left-1/2 -ml-56">Nepoleia is solde out!</h2>
-      <div className="absolute top-80 left-1/2 -ml-32"><DiscordBtn /> </div>
+      <div className="absolute top-96 left-1/2 -ml-32"><DiscordBtn /> </div>
     </div>
   );
 };
