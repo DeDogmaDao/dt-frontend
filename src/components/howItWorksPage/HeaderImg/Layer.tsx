2a1d2469b0b3817110677386109abd491f1f8ab7
aly3n <aly3n.dev@gmail.com>
2022-02-26 17:31:41 +0330

    scale added to layers

--- src/components/howItWorksPage/HeaderImg/Layer.tsx
+++ src/components/howItWorksPage/HeaderImg/Layer.tsx
@@ -7,10 +7,16 @@ interface props {
   index: number;
 }
 const Layer: React.FC<props> = ({ data, top, index }) => {
+  const scales = data.image === "2.png" ? `scale(${1 - top / 100})` : "";
+  const translates = -top * index + index * 35;
+  const styles = {
+    transform: `translateY(${translates}px) ${scales}`,
+  };
+
   return (
     <img
       src={"/media/layers/" + data.image}
-      style={{ transform: `translateY(${-top * index + index * 35}px) ${data.image==="2.png" ? `scale(${1 - top/100})` : ""}` }}
+      style={styles}
       className={` transform-gpu ${
         index === 0 ? "w-full h-full" : "absolute left-0 top-0 w-full h-full"
       }
