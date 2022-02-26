214b432bc041fc1b1037cb1cf6cecd9e03e22b07
aly3n <aly3n.dev@gmail.com>
2022-02-26 16:50:54 +0330

    scale for stone added

--- src/components/howItWorksPage/HeaderImg/Layer.tsx
+++ src/components/howItWorksPage/HeaderImg/Layer.tsx
@@ -1,21 +1,20 @@
 import { layersType } from "../../../types/propTypes";
-import {motion} from 'framer-motion';
+import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
 interface props {
   data: layersType;
   top: number;
-  index:number;
+  index: number;
 }
-const Layer: React.FC<props> = ({ data, top,index }) => {
-
-    
+const Layer: React.FC<props> = ({ data, top, index }) => {
   return (
-    <img 
+    <img
       src={"/media/layers/" + data.image}
-      style={{transform:`translateY(${-top * index + index * 35}px)`}}
+      style={{ transform: `translateY(${-top * index + index * 35}px) ${data.image==="2.png" ? `scale(${1 - top/100})` : ""}` }}
       className={` transform-gpu ${
         index === 0 ? "w-full h-full" : "absolute left-0 top-0 w-full h-full"
-      }`}
+      }
+      `}
     />
   );
 };
