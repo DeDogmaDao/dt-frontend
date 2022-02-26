70028bf7a15b85c60a7aa02aa662a47318c8a29a
aly3n <aly3n.dev@gmail.com>
2022-02-26 22:09:10 +0330

    shadow translate

--- src/components/howItWorksPage/HeaderImg/index.tsx
+++ src/components/howItWorksPage/HeaderImg/index.tsx
@@ -4,27 +4,26 @@ import DiscordBtn from "../../global/Buttons/DiscordBtn";
 import Layer from "./Layer";
 
 const HeaderImg: React.FC = () => {
-
-
   const [top, setTop] = useState(0);
-    const topTransform = () => {
-        setTop(window.scrollY / 15)
-        console.log(top);
-    }
-    useEffect(() => {
-        window.addEventListener("scroll",topTransform)
-        return () => {
-        window.removeEventListener("scroll",topTransform)
-        }
-    }, [top])
-
+  const topTransform = () => {
+    setTop(window.scrollY / 15);
+    console.log(top);
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", topTransform);
+    return () => {
+      window.removeEventListener("scroll", topTransform);
+    };
+  }, [top]);
 
   return (
     <div className="w-full h-full flex justify-center items-center relative -mt-16">
       {layersData.map((data, index) => {
         return <Layer top={top} index={index} data={data} />;
       })}
-      <div className="bg-gradient-to-b from-transparent via-mainBg-500 to-mainBg-500 w-full h-[450px] absolute left-0 top-[calc(25%+392px)] z-100" />
+      <div className="bg-gradient-to-b from-transparent via-mainBg-500 to-mainBg-500 w-full h-[450px] absolute left-0 
+      top-[calc(25%+392px)] "
+      style={{ transform: `translateY(${(35-top)*15}px)`}} />
       <h2 className="absolute top-56 left-1/2 -ml-56">Nepoleia is sold out!</h2>
       <div className="absolute top-96 left-1/2 -ml-32">
         <DiscordBtn />{" "}
