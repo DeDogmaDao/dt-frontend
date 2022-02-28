335696af2e1780de3d7a13595b65268ca79cee4a
aly3n <aly3n.dev@gmail.com>
2022-03-01 03:01:04 +0330

    for deploy

--- src/components/howItWorksPage/Tabs/Card.tsx
+++ src/components/howItWorksPage/Tabs/Card.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, RefObject } from "react";
+import { ReactElement, RefObject, useEffect, useRef } from "react";
 import { tabType, teamType } from "../../../types/allTypes";
 
 interface props {
@@ -7,9 +7,13 @@ cardRef: RefObject<HTMLDivElement>;
 
 }
 const Card:React.FC<props> = ({data, cardRef}) => {
+const eachCradRef = useRef<HTMLDivElement>(null);
+useEffect(() => {
+    // const scaleRatio = cardRef.current!.offsetLeft - eachCradRef.current!.offsetLeft;
+}, [])
 
     return(
-        <div ref={data.activeCard ? cardRef : null} className="h-full py-5 w-80 flex flex-col justify-center items-center text-white">
+        <div ref={data.activeCard ? cardRef : eachCradRef} className="h-full py-5 w-80 flex flex-col justify-center items-center text-white">
             <img className="w-full object-contain" src={data.image} loading="lazy" />
             <p className="text-4xl text-primary-500">{data.name}</p>
             {/* <div>{data.desc}</div> */}
