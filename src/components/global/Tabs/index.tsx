cdae8a7746a926ea3e6bb7defe3129c855f9c156
aly3n <aly3n.dev@gmail.com>
2022-03-01 03:03:10 +0330

    error fixed

--- src/components/global/Tabs/index.tsx
+++ src/components/global/Tabs/index.tsx
@@ -1,5 +1,5 @@
 import { createRef, Ref, useRef } from "react";
-import { tabsData } from "../../../store/allData";
+import { allTabs } from "../../../store/allData";
 import Button from "../Buttons/Button";
 
 const Tabs: React.FC = () => {
@@ -8,8 +8,8 @@ const Tabs: React.FC = () => {
   return (
     <div className="flex flex-col justify-between items-center bg-blackPrime h-screen py-20">
       <div className="flex justify-center items-center gap-x-5 bg-mainBg-500 h-10 px-1 rounded-md">
-        {tabsData.map((button) => {
-          return <Button ref={ref} content={button.name} customClass={"rounded-md"} />;
+        {allTabs.map((button) => {
+          return <Button ref={ref} content={button.tabGroup} customClass={"rounded-md"} />;
         })}
       </div>
       <div></div>
