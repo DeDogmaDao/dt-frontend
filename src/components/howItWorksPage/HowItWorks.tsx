e8d9e7d2aa49faa8f41d8ebeef84722a66c6d0a0
aly3n <aly3n.dev@gmail.com>
2022-03-03 00:39:23 +0330

    for test door

--- src/components/howItWorksPage/HowItWorks.tsx
+++ src/components/howItWorksPage/HowItWorks.tsx
@@ -5,22 +5,32 @@ import Card from "../global/Card";
 import HeaderImg from "./HeaderImg";
 import SecOne from "./SecOne";
 import Tabs from "./Tabs";
+import { motion } from "framer-motion";
 
-const teamClone = [...teamData,...teamData,...teamData,...teamData,...teamData,]
-const HowItWorks:React.FC = () => {
+const teamClone = [
+  ...teamData,
+  ...teamData,
+  ...teamData,
+  ...teamData,
+  ...teamData,
+];
+const HowItWorks: React.FC = () => {
+  return (
+    <>
+      <HeaderImg />
 
+      <SecOne />
 
-    return(
-        <>
-        <HeaderImg />
+      <div className="relative h-screen w-screen my-80 flex justify-center items-center">
+        <img src="/media/junks/Door.png" className="h-full" />
+        <motion.img src="/media/team/member1.png" className="absolute w-16 -bottom-5  left-[43%]" style={{transform:"rotateX(69deg) skew(-19deg)"}} />
+      </div>
 
-        <SecOne />
-        <Carosel allData={teamClone} Item={Card} initialQuantity={100} />
-        <FAQ />
-        <Tabs />
-        </>
-    )
-}
-
+      <Carosel allData={teamClone} Item={Card} initialQuantity={100} />
+      <FAQ />
+      <Tabs />
+    </>
+  );
+};
 
 export default HowItWorks;
