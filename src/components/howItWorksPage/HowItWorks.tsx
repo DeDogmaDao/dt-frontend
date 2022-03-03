9cea3e56bbeb2e3af679bac25cb3daf87aa03cc0
aly3n <aly3n.dev@gmail.com>
2022-03-03 03:33:28 +0330

    remove test

--- src/components/howItWorksPage/HowItWorks.tsx
+++ src/components/howItWorksPage/HowItWorks.tsx
@@ -5,51 +5,22 @@ import Card from "../global/Card";
 import HeaderImg from "./HeaderImg";
 import SecOne from "./SecOne";
 import Tabs from "./Tabs";
-import { motion } from "framer-motion";
 
-const teamClone = [
-  ...teamData,
-  ...teamData,
-  ...teamData,
-  ...teamData,
-  ...teamData,
-];
-const HowItWorks: React.FC = () => {
-  return (
-    <>
-      <HeaderImg />
+const teamClone = [...teamData,...teamData,...teamData,...teamData,...teamData,]
+const HowItWorks:React.FC = () => {
 
-      <SecOne />
 
-      <div className="relative h-screen w-screen my-80 flex justify-center items-center">
-        <img src="/media/junks/Door.png" className="h-full" />
-        <motion.img
-          src="/media/team/member1.png"
-          className="absolute w-16 -bottom-5  left-[43%]"
-          style={{ transform: "rotateX(69deg) skew(-19deg)" }}
-        />
-        <motion.img
-          src="/media/team/member2.png"
-          className="absolute w-16 -bottom-5  left-[50%]"
-          style={{ transform: "rotateX(69deg) rotateZ(-12deg)" }}
-        />
-        <motion.img
-          src="/media/team/member3.png"
-          className="absolute w-10 bottom-7  left-[45%]"
-          style={{ transform: "rotateX(69deg) skew(-19deg)" }}
-        />
-        <motion.img
-          src="/media/team/member1.png"
-          className="absolute w-10 bottom-7  left-[50%]"
-          style={{ transform: "rotateX(69deg) skew(11deg)" }}
-        />
-      </div>
+    return(
+        <>
+        <HeaderImg />
 
-      <Carosel allData={teamClone} Item={Card} initialQuantity={100} />
-      <FAQ />
-      <Tabs />
-    </>
-  );
-};
+        <SecOne />
+        <Carosel allData={teamClone} Item={Card} initialQuantity={100} />
+        <FAQ />
+        <Tabs />
+        </>
+    )
+}
+
 
 export default HowItWorks;
