57990d78f58b20589a12427dcd7b8a037250f9b9
aly3n <aly3n.dev@gmail.com>
2022-03-05 17:35:30 +0330

    bg added to game board, refactoring to layoutgroup

--- src/components/gameBoardPage/GameBoard.tsx
+++ src/components/gameBoardPage/GameBoard.tsx
@@ -6,7 +6,7 @@ const GameBoard: React.FC = () => {
   const [isStarted, setIsStarted] = useState<number | null>(null);
   return (
     <div className="flex justify-between items-center w-screen h-screen">
-      <div className="w-1/3 h-full bg-green-900 flex justify-center items-start flex-wrap gap-3 relative">
+      <div className="w-1/3 h-full bg-green-900 flex justify-center items-start flex-wrap gap-3 relative z-10">
         {gameCardData.map((data, index) => {
           return (
             <CardGame
@@ -18,7 +18,12 @@ const GameBoard: React.FC = () => {
           );
         })}
       </div>
-      <div className="w-2/3 h-full bg-red-900"></div>
+      <div className="w-2/3 h-full bg-red-900 flex justify-center items-start relative z-0">
+        <img
+          src="/media/bg/game2.png"
+          className="absolute top-0 left-0 w-full h-full"
+        />
+      </div>
     </div>
   );
 };
