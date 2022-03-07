a1034e2d2642aaab376db109ebf6e9af66e15ccf
aly3n <aly3n.dev@gmail.com>
2022-03-07 14:07:21 +0330

    bottom spell fixed

--- src/components/gameBoardPage/GameBoard.tsx
+++ src/components/gameBoardPage/GameBoard.tsx
@@ -8,22 +8,22 @@ const spells: number[] = Array.from(Array(40).keys());
 const topSpells: number[] = Array.from(Array(10).keys());
 const bottomPosition = {
   top: 118.5,
-  right: 429,
+  right: 428.5,
   width: 420,
   height: 202,
-}
+};
 const bottomRightSpell = {
   bottom: 22.75,
   right: 4.4,
   width: 0.2,
   height: 0.2,
-}
+};
 const bottomLeftSpell = {
   bottom: 22.75,
   right: 4.55,
   width: 0.2,
   height: 0.2,
-}
+};
 
 const GameBoard: React.FC = () => {
   const [turnNumber, setTurnNumber] = useState<number | null>(null);
@@ -86,36 +86,11 @@ const GameBoard: React.FC = () => {
           />
           <DoorSpells
             spells={spells}
-            right={(418 / 1920) * 100}
+            right={(419 / 1920) * 100}
             containerPosition={bottomPosition}
             spellPosition={bottomLeftSpell}
           />
-          <div
-            className="absolute z-100"
-            style={{
-              width: `${(420 / 1920) * 100}vw`,
-              right: `${(429 / 1920) * 100}vw`,
-              height: `${(202 / 900) * 100}vw`,
-              top: `${(118 / 900) * 100}vw`,
-            }}
-          >
-            <div className="relative w-full h-full">
 
-              {spells.map((spell) => {
-                return (
-                  <div
-                    className="rounded-full  absolute"
-                    style={{
-                      right: (415 / 1920) * 100 - spell * (4.1 / 37) + "vw",
-                      bottom: 0 + spell * (21.1 / 37) + "vw",
-                      width: 0.2 + "vw",
-                      height: 0.2 + "vw",
-                    }}
-                  ></div>
-                );
-              })}
-            </div>
-          </div>
           <div className="absolute left-[30%] top-[8%] w-full h-full">
             <div
               className="relative w-full h-full"
