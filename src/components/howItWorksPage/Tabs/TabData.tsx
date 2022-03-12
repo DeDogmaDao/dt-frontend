059a34ff3ee4676e63f19c83286488788f5edf3f
aly3n <aly3n.dev@gmail.com>
2022-03-12 03:39:04 +0330

    tabData added

---
+++ src/components/howItWorksPage/Tabs/TabData.tsx
@@ -0,0 +1,12 @@
+const TabData:React.FC = () => {
+
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+        <img />
+        <div></div>
+        </div>
+    );
+}
+
+
+export default TabData;
