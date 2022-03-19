74dbaa4fd4557a58df7dd5f1e4c1df0f73371709
aly3n <aly3n.dev@gmail.com>
2022-03-19 17:57:56 +0330

    calculation added

---
+++ src/components/gameBoardPage/Calculation.tsx
@@ -0,0 +1,16 @@
+const Calculation:React.FC = () => {
+
+
+    return(
+        <div className="flex flex-col justify-center items-center absolute left-[5vw] bottom-[5vw]">
+
+        <div className="flex justify-between">12 × 6 = 10</div>
+
+        <div className="flex justify-between">1222 × 653 = 1054</div>
+
+        </div>
+    );
+}
+
+
+export default Calculation;
