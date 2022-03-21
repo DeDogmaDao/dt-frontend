b7df6bc663d2ff227de5ff85cd45dcf1eb179079
aly3n <aly3n.dev@gmail.com>
2022-03-21 10:30:22 +0330

    calculation bug fixed

--- src/components/gameBoardPage/Calculation.tsx
+++ src/components/gameBoardPage/Calculation.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { gameCardType } from "../../types/allTypes";
 import CardNum from "./CardNum";
 interface props {
@@ -7,15 +7,17 @@ interface props {
 }
 
 const communityNumStyles: React.CSSProperties = {
-  left: "0.3vw",
+  left: "0vw",
   bottom: "0vw",
 };
 const individualNumStyles: React.CSSProperties = {
-  right: "0.5vw",
+  right: "0vw",
   bottom: "0vw",
 };
 
 const Calculation: React.FC<props> = ({ firstCardNum, currentCard }) => {
+  const communityControls = useAnimation();
+  const individualControls = useAnimation();
   return (
     <>
       <motion.div
@@ -24,27 +26,32 @@ const Calculation: React.FC<props> = ({ firstCardNum, currentCard }) => {
       >
         {firstCardNum}
       </motion.div>
-      <div className="flex flex-col justify-center items-center absolute left-[7vw] bottom-[2.2vw] text-4xl">
-        <div className="flex justify-between">
-          <motion.div>12</motion.div> ×
-          <CardNum
-            num={currentCard?.communityNum}
-            styles={communityNumStyles}
-            layoutID="communityNum"
-            showHidden={false}
-          />
+      <div className="flex flex-col justify-start items-center absolute left-[6.5vw] bottom-[2.6vw] text-4xl bg-red-400 w-[20vw] h-[5vw]">
+        <div className="flex justify-between w-full">
+          <div className="w-full bg-lime-300 "></div>
+          ×
+          <motion.div className="w-full bg-purple-300 relative flex justify-center items-center" initial="hidden" animate={communityControls}>
+            <CardNum
+              num={currentCard?.communityNum}
+              styles={communityNumStyles}
+              layoutID="communityNum"
+              showHidden={false}
+            />
+          </motion.div>
           +
-          <CardNum
-            num={currentCard?.individualNum}
-            styles={individualNumStyles}
-            layoutID="individualNum"
-            showHidden={false}
-          />
-          <motion.div layoutId="individualNum">7987</motion.div> = <div>10</div>
+          <motion.div className="w-full bg-blue-300 relative" initial="hidden" animate={individualControls}>
+            <CardNum
+              num={currentCard?.individualNum}
+              styles={individualNumStyles}
+              layoutID="individualNum"
+              showHidden={false}
+            />
+          </motion.div>
+          = <div>10</div>
         </div>
 
         <div className="flex justify-between">
-          <div>1222</div> % <div>653</div> = <div>1054</div>
+          {/* <div>1222</div> % <div>653</div> = <div>1054</div> */}
         </div>
       </div>
     </>
