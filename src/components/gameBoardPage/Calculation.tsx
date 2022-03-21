e154622b4c465c847d57d7af52b5c48e1009e3d8
aly3n <aly3n.dev@gmail.com>
2022-03-21 14:05:43 +0330

    calculation refactor

--- src/components/gameBoardPage/Calculation.tsx
+++ src/components/gameBoardPage/Calculation.tsx
@@ -1,5 +1,6 @@
 import { motion, useAnimation } from "framer-motion";
 import { gameCardType } from "../../types/allTypes";
+import { calcFirstResultAni } from "../../utils/animation";
 import CardNum from "./CardNum";
 interface props {
   firstCardNum: number;
@@ -12,6 +13,7 @@ const individualNumStyles: React.CSSProperties = {};
 const Calculation: React.FC<props> = ({ firstCardNum, currentCard }) => {
   const communityControls = useAnimation();
   const individualControls = useAnimation();
+
   return (
     <>
       <motion.div
@@ -20,11 +22,14 @@ const Calculation: React.FC<props> = ({ firstCardNum, currentCard }) => {
       >
         {firstCardNum}
       </motion.div>
-      <div className="flex flex-col justify-start items-center absolute left-[6.5vw] bottom-[2.6vw] text-4xl bg-red-400 w-[20vw] h-[5vw]">
-        <div className="flex justify-between w-full">
-          <div className="w-full bg-lime-300 "></div>×
+      <div className="flex flex-col justify-start items-center absolute left-[5.5vw] bottom-[2.6vw] text-2xl bg-red-400 w-[22vw] h-[5vw]">
+        <div className="w-[4.5vw] h-[2.5vw] bg-lime-300 absolute left-0 top-0"></div>
+        <div className="w-[1.5vw] h-[2.5vw] bg-gray-300 absolute left-[4.5vw] top-0 text-center">
+          ×
+        </div>
+        <div className="w-[3vw] h-[2.5vw] bg-blue-300 absolute left-[6vw] top-0 text-center">
           <motion.div
-            className="w-full bg-purple-300 relative flex justify-center items-center"
+            className="w-full h-full relative flex justify-center items-center"
             initial="hidden"
             animate={communityControls}
           >
@@ -35,9 +40,13 @@ const Calculation: React.FC<props> = ({ firstCardNum, currentCard }) => {
               showHidden={false}
             />
           </motion.div>
+        </div>
+        <div className="w-[1.5vw] h-[2.5vw] bg-gray-300 absolute left-[9vw] top-0 text-center">
           +
+        </div>
+        <div className="w-[3vw] h-[2.5vw] bg-purple-300 absolute left-[10.5vw] top-0 text-center">
           <motion.div
-            className="w-full bg-blue-300 relative flex justify-center items-center"
+            className="w-full h-full relative flex justify-center items-center"
             initial="hidden"
             animate={individualControls}
           >
@@ -48,12 +57,32 @@ const Calculation: React.FC<props> = ({ firstCardNum, currentCard }) => {
               showHidden={false}
             />
           </motion.div>
-          = <div>10</div>
+        </div>
+        <div className="w-[1.5vw] h-[2.5vw] bg-gray-300 absolute left-[13.5vw] top-0 text-center">
+          =
+        </div>
+        <div className="w-[7vw] h-[2.5vw] bg-orange-300 absolute left-[15vw] top-0 text-center">
+          <motion.div
+            className="relative flex justify-center items-center h-full w-full"
+            initial="hidden"
+            animate="visible"
+            variants={calcFirstResultAni}
+          >
+            {currentCard &&
+              currentCard?.cardNum * currentCard?.communityNum +
+                currentCard?.individualNum}
+          </motion.div>
         </div>
 
-        <div className="flex justify-between">
-          {/* <div>1222</div> % <div>653</div> = <div>1054</div> */}
+        <div className="w-[6.5vw] h-[2.5vw] bg-green-300 absolute left-[0vw] top-[2.5vw] text-center"></div>
+        <div className="w-[1.5vw] h-[2.5vw] bg-gray-300 absolute left-[6.5vw] top-[2.5vw] text-center">
+          %
         </div>
+        <div className="w-[5.5vw] h-[2.5vw] bg-red-300 absolute left-[8vw] top-[2.5vw] text-center"></div>
+        <div className="w-[1.5vw] h-[2.5vw] bg-gray-300 absolute left-[13.5vw] top-[2.5vw] text-center">
+          %
+        </div>
+        <div className="w-[7vw] h-[2.5vw] bg-blue-300 absolute left-[15vw] top-[2.5vw] text-center"></div>
       </div>
     </>
   );
