3fd991712eecb53ba9ff4bfd6fb08be2d33c6db2
aly3n <aly3n.dev@gmail.com>
2022-03-28 02:14:13 +0430

    fix for tab cycle

--- src/components/howItWorksPage/Tabs/TabData.tsx
+++ src/components/howItWorksPage/Tabs/TabData.tsx
@@ -24,7 +24,7 @@ const TabData: React.FC<props> = ({ data, activeGroup }) => {
               variants={tabImageContainerAni}
               className="h-full w-[300px] relative"
             >
-              <motion.div className="w-[300px] absolute -bottom-7 -left-10 h-[50%]  rounded-tl-full rounded-tr-full z-20 overflow-hidden">
+              <motion.div className="w-[390px] absolute -bottom-7 left-[-29%] h-[58%]  rounded-tl-full rounded-tr-full z-20 overflow-hidden">
                 <motion.img variants={tabImageButtomAni}               initial="hidden"
               animate="visible" src={'/media/junks/tabCycle.png'}  />
               </motion.div>
