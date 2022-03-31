e8a86d7d25ea72bd0a2d86b2102f58c3645719d9
aly3n <aly3n.dev@gmail.com>
2022-03-31 17:43:12 +0430

    translates added to ring and pin

--- src/components/gameBoardPage/Door.tsx
+++ src/components/gameBoardPage/Door.tsx
@@ -1,5 +1,5 @@
 import { motion, MotionStyle } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { spellNumber } from "../../types/allTypes";
 
 interface props {
@@ -16,6 +16,20 @@ const Door: React.FC<props> = ({ spellNumber }) => {
     ring: {} as MotionStyle,
   });
 
+  useEffect(() => {
+    setTimeout(() => {
+      const plusOrMinus = spellNumber.blue - spellNumber.yellow > 0 ? -1 : 1;
+      const delta: number = 10;
+      setStyles({
+        ring: { translateX: delta / 3 + "vw" },
+        topPin: { translateY: (plusOrMinus * Math.pow(delta, 2)) / 28 + "vw" },
+        bottomPin: {
+          translateY: (plusOrMinus * -Math.pow(delta, 2)) / 35 + "vw",
+        },
+      });
+    }, 3000);
+  }, [spellNumber]);
+
   return (
     <div className="absolute  top-[10.53vw] left-[59.05vw] w-[15.391vw] h-[26vw] bg-red-500">
       <div className="relative w-full h-full">
@@ -28,7 +42,10 @@ const Door: React.FC<props> = ({ spellNumber }) => {
           className="absolute bottom-0 left-0 w-[7.7vw] h-full z-0 scale-x-[-1]"
         />
       </div>
-      <div className="absolute top-[12.5vw] left-1/2 ml-[-0.7vw] w-[1.4vw] h-[1.4vw]  z-10">
+      <motion.div
+        style={styles.ring}
+        className="absolute top-[12.5vw] left-1/2 ml-[-0.7vw] w-[1.4vw] h-[1.4vw] z-10 duration-2000"
+      >
         <div className="relative w-full h-full">
           <img
             src="/media/game/ring.png"
@@ -36,16 +53,17 @@ const Door: React.FC<props> = ({ spellNumber }) => {
           />
 
           <motion.img
-            style={}
+            style={styles.topPin}
             src="/media/game/pin.png"
-            className="absolute top-[-0.5vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10"
+            className="absolute top-[-0.5vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 duration-2000"
           />
           <motion.img
+            style={{ ...styles.bottomPin, scaleY: -1 }}
             src="/media/game/pin.png"
-            className="absolute bottom-[-0.5vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 scale-y-[-1]"
+            className="absolute bottom-[-0.6vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 duration-2000"
           />
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
