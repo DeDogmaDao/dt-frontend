0b329ce0c1154f6640647e5beffda50a45956d86
aly3n <aly3n.dev@gmail.com>
2022-03-31 19:00:36 +0430

    RingPin.tsx added

--- src/components/gameBoardPage/Door.tsx
+++ src/components/gameBoardPage/Door.tsx
@@ -1,35 +1,13 @@
 import { motion, MotionStyle } from "framer-motion";
 import { useEffect, useState } from "react";
 import { spellNumber } from "../../types/allTypes";
+import RingPin from "./RingPin";
 
 interface props {
   spellNumber: spellNumber;
 }
 
-let topPinStyles = (constant: number, BlueSpellNumber: number) => {
-  return {} as MotionStyle;
-};
 const Door: React.FC<props> = ({ spellNumber }) => {
-  const [styles, setStyles] = useState({
-    topPin: {} as MotionStyle,
-    bottomPin: {} as MotionStyle,
-    ring: {} as MotionStyle,
-  });
-
-  useEffect(() => {
-    setTimeout(() => {
-      const plusOrMinus = spellNumber.blue - spellNumber.yellow >= 0 ? -1 : 1;
-      const delta: number = spellNumber.blue - spellNumber.yellow;
-      setStyles({
-        ring: { translateX: delta / 3 + "vw" },
-        topPin: { translateY: (plusOrMinus * Math.pow(delta, 2)) / 350 + "vw" },
-        bottomPin: {
-          translateY: (plusOrMinus * -Math.pow(delta, 2)) / 400 + "vw",
-        },
-      });
-    }, 3000);
-  }, [spellNumber]);
-
   return (
     <div className="absolute  top-[10.53vw] left-[59.05vw] w-[15.391vw] h-[26vw] bg-red-500">
       <div className="relative w-full h-full">
@@ -42,28 +20,7 @@ const Door: React.FC<props> = ({ spellNumber }) => {
           className="absolute bottom-0 left-0 w-[7.7vw] h-full z-0 scale-x-[-1]"
         />
       </div>
-      <motion.div
-        style={styles.ring}
-        className="absolute top-[12.5vw] left-1/2 ml-[-0.7vw] w-[1.4vw] h-[1.4vw] z-10 duration-2000"
-      >
-        <div className="relative w-full h-full">
-          <img
-            src="/media/game/ring.png"
-            className="absolute bottom-0 left-0 w-full h-full"
-          />
-
-          <motion.img
-            style={styles.topPin}
-            src="/media/game/pin.png"
-            className="absolute top-[-0.5vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 duration-2000"
-          />
-          <motion.img
-            style={{ ...styles.bottomPin, scaleY: -1 }}
-            src="/media/game/pin.png"
-            className="absolute bottom-[-0.6vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 duration-2000"
-          />
-        </div>
-      </motion.div>
+      <RingPin spellNumber={spellNumber} />
     </div>
   );
 };
---
+++ src/components/gameBoardPage/RingPin.tsx
@@ -0,0 +1,55 @@
+import { motion, MotionStyle } from "framer-motion";
+import { useEffect, useState } from "react";
+import { spellNumber } from "../../types/allTypes";
+
+interface props {
+  spellNumber: spellNumber;
+}
+const RingPin: React.FC<props> = ({ spellNumber }) => {
+  const [styles, setStyles] = useState({
+    topPin: {} as MotionStyle,
+    bottomPin: {} as MotionStyle,
+    ring: {} as MotionStyle,
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      const plusOrMinus = spellNumber.blue - spellNumber.yellow >= 0 ? -1 : 1;
+      const delta: number = spellNumber.blue - spellNumber.yellow;
+      setStyles({
+        ring: { translateX: delta / 3 + "vw" },
+        topPin: { translateY: (plusOrMinus * Math.pow(delta, 2)) / 350 + "vw" },
+        bottomPin: {
+          translateY: (plusOrMinus * -Math.pow(delta, 2)) / 400 + "vw",
+        },
+      });
+    }, 3000);
+  }, [spellNumber]);
+
+  return (
+    <motion.div
+      style={styles.ring}
+      className="absolute top-[12.5vw] left-1/2 ml-[-0.7vw] w-[1.4vw] h-[1.4vw] z-10 duration-2000"
+    >
+      <div className="relative w-full h-full">
+        <img
+          src="/media/game/ring.png"
+          className="absolute bottom-0 left-0 w-full h-full"
+        />
+
+        <motion.img
+          style={styles.topPin}
+          src="/media/game/pin.png"
+          className="absolute top-[-0.5vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 duration-2000"
+        />
+        <motion.img
+          style={{ ...styles.bottomPin, scaleY: -1 }}
+          src="/media/game/pin.png"
+          className="absolute bottom-[-0.6vw] left-1/2 ml-[-0.33vw] w-[0.66vw] z-10 duration-2000"
+        />
+      </div>
+    </motion.div>
+  );
+};
+
+export default RingPin;
