6917c271cd3e217974c05a0d72695083522ea2ff
aly3n <aly3n.dev@gmail.com>
2022-04-02 21:51:22 +0430

    FEATURE(next.config.js):redirects to construction page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 module.exports = {
-    options: {
-      sourcemaps: 'development' // possible values can be production, development, or none
-    },
-    swcMinify: true,
-  }
+  options: {
+    sourcemaps: "development", // possible values can be production, development, or none
+  },
+  swcMinify: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/underconstruction",
+        permanent: true,
+      },
+      {
+        source: "/gallery",
+        destination: "/underconstruction",
+        permanent: true,
+      },
+      {
+        source: "/gameboard",
+        destination: "/underconstruction",
+        permanent: true,
+      },
+    ];
+  },
+};
