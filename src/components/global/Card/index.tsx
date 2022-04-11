a2b25f1a70d76d511b73a5a9484a081c8e540935
aly3n <aly3n.dev@gmail.com>
2022-04-12 02:30:10 +0430

    feat(team): hover transition added to cards

--- src/components/global/Card/index.tsx
+++ src/components/global/Card/index.tsx
@@ -1,20 +1,27 @@
 import Image from "next/image";
 import { globCardType, sizeType } from "../../../types/allTypes";
-
+import { motion } from "framer-motion";
 interface props {
-    data:globCardType;
-    size:sizeType;
+  data: globCardType;
+  size: sizeType;
 }
-const Card:React.FC<props> = ({data, size}) => {
-
-    return(
-        <div  className="flex flex-col justify-center items-center text-white">
-            <Image width={size.width} height={size.height}  src={data.image} loading="lazy" />
-            {data.name && <p className="text-medium-bold text-white mt-4">{data.name}</p>}
-            {data.role && <p className="text-small-light mt-2">{data.role}</p>}
-        </div>
-    );
-}
-
+const Card: React.FC<props> = ({ data, size }) => {
+  return (
+    <motion.div className="flex flex-col justify-center items-center text-white group">
+      <div style={{ ...size }} className="group-hover:-translate-y-3 group-hover:opacity-70 duration-500 ">
+        <Image
+          width={size.width}
+          height={size.height}
+          src={data.image}
+          loading="lazy"
+        />
+      </div>
+      {data.name && (
+        <p className="text-medium-bold text-white mt-4 group-hover:text-primary-500 duration-500">{data.name}</p>
+      )}
+      {data.role && <p className="text-small-light mt-2">{data.role}</p>}
+    </motion.div>
+  );
+};
 
 export default Card;
--- src/components/teamPage/Team.tsx
+++ src/components/teamPage/Team.tsx
@@ -9,16 +9,16 @@ const Team:React.FC = () => {
             <div className="w-full h-full absolute top-0 left-0">
             <Image  src={'/media/team/team-bg.png'} width="1438" height="1128" layout="responsive" />
             </div>
-            <div className="relative w-full h-full  flex flex-col justify-start items-center pt-44">
+            <div className="relative w-full h-full  flex flex-col justify-start items-center pt-44 pb-10">
                 <h3>
                 Meet the Creators
                 </h3>
-                <div className="flex justify-center items-center w-full gap-x-8 mt-20">
+                <div className="flex flex-wrap justify-center items-center w-full gap-8 mt-20">
                     {teamDataLvl1.map(member=>{
                         return <Card data={member} size={{width:200,height:260}} />
                     })}
                 </div>
-                <div className="flex justify-center items-center w-full gap-x-8 mt-8">
+                <div className="flex flex-wrap justify-center items-center w-full gap-8 mt-8">
                     {teamDataLvl2.map(member=>{
                         return <Card data={member} size={{width:200,height:260}} />
                     })}
