6b621b5daf5432273146639b6bbd27b3be5e8448
aly3n <aly3n.dev@gmail.com>
2022-04-11 23:46:19 +0430

    feat(roadmap):road component added to raodmap page

--- src/components/roadMapPage/RoadMapPage.tsx
+++ src/components/roadMapPage/RoadMapPage.tsx
@@ -1,5 +1,11 @@
+import Road from "./Road";
+
 const RoadMapPage: React.FC = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-start items-center w-full h-full">
+      <Road />
+    </div>
+  );
 };
 
 export default RoadMapPage;
