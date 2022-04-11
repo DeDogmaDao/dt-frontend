9e4612e74a560fba13019ce9571e74b3582890c5
aly3n <aly3n.dev@gmail.com>
2022-04-11 22:56:07 +0430

    feat(roadmap):Road component added

---
+++ src/components/roadMapPage/Road.tsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion";
+
+const Road: React.FC = () => {
+  return (
+    <div className="w-screen h-screen">
+      <div className="flex justify-between items-center">
+          
+      </div>
+    </div>
+  );
+};
+
+export default Road;
