a171c5ecd4f023d12f66c8db4e52af103aaaaa50
aly3n <aly3n.dev@gmail.com>
2022-04-12 15:06:24 +0430

    feat(roadmap):box model added

--- src/components/roadMapPage/Road.tsx
+++ src/components/roadMapPage/Road.tsx
@@ -1,10 +1,14 @@
 import { motion } from "framer-motion";
 
-const Road: React.FC = () => {
+interface props {
+  color:string;
+}
+const Road: React.FC<props> = ({color}) => {
   return (
     <div className="w-screen h-screen">
-      <div className="flex justify-between items-center">
-          
+      <div className="flex justify-between items-center w-full h-full">
+          <div className="w-1/2 h-full " style={{backgroundColor:color}}></div>
+          <div className="w-1/2 h-full bg-green-300"></div>
       </div>
     </div>
   );
--- src/components/roadMapPage/RoadMapPage.tsx
+++ src/components/roadMapPage/RoadMapPage.tsx
@@ -1,9 +1,15 @@
 import Road from "./Road";
-
+const colors = [
+  "#f542e9",
+  "#5c485a",
+  "#1b7a29",
+]
 const RoadMapPage: React.FC = () => {
   return (
     <div className="flex flex-col justify-start items-center w-full h-full">
-      <Road />
+      {colors.map(color=>{
+        return <Road color={color} />
+      })}
     </div>
   );
 };
