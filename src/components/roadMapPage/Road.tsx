473683c559c821ca621b43fa9fd7e9e3a2791558
aly3n <aly3n.dev@gmail.com>
2022-04-13 16:45:48 +0430

    feat(roadmap): road item style added

--- src/components/roadMapPage/Road.tsx
+++ src/components/roadMapPage/Road.tsx
@@ -1,14 +1,36 @@
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import RoadItem from "./RoadItem";
 
 interface props {
   color: string;
   index: number;
   activeSection: number;
 }
+const roadData = [
+  {
+    title: "Set and get their state.",
+    desc: "All motion components internally use MotionValues to track the state and velocity of an animating value.Usually, these are created automatically. But for advanced use-cases, it is possible to create them manually and inject them into motion components.",
+  },
+  {
+    title: "Any MotionValue can spawn ",
+    desc: "All motion components internally use MotionValues to track the state and velocity of an animating value.Usually, these are created automatically. But for advanced use-cases, it is possible to create them manually and inject them into motion components.",
+  },
+  {
+    title: "Chain MotionValues via the useTransform hook.",
+    desc: "All motion components internally use MotionValues to track the state and velocity of an animating value.Usually, these are created automatically. But for advanced use-cases, it is possible to create them manually and inject them into motion components.",
+  },
+  {
+    title: "before returning it to update the child.",
+    desc: "All motion components internally use MotionValues to track the state and velocity of an animating value.Usually, these are created automatically. But for advanced use-cases, it is possible to create them manually and inject them into motion components.",
+  },
+  {
+    title: "Set and get their state.",
+    desc: "All motion components internally use MotionValues to track the state and velocity of an animating value.Usually, these are created automatically. But for advanced use-cases, it is possible to create them manually and inject them into motion components.",
+  },
+];
 const Road: React.FC<props> = ({ color, index, activeSection }) => {
   const [clientHeight, setClientHeight] = useState(0);
-
   useEffect(() => {
     setClientHeight(window.innerHeight);
   }, []);
@@ -19,10 +41,12 @@ const Road: React.FC<props> = ({ color, index, activeSection }) => {
       style={{ top: clientHeight * (index - activeSection) }}
     >
       <div className="flex justify-between items-center w-full h-full">
-        <div className="w-1/2 h-full" ></div>
+        <div className="w-1/2 h-full"></div>
         <div className="w-1/2 h-full flex justify-center items-center">
-          <ul className="w-[416px] p-8 ">
-
+          <ul className="w-[416px] p-8 flex flex-col gap-y-2 bg-neutral-600 rounded-xl">
+            {roadData.map(data => {
+              return <RoadItem data={data} />
+            })}
           </ul>
         </div>
       </div>
--- src/components/roadMapPage/RoadItem.tsx
+++ src/components/roadMapPage/RoadItem.tsx
@@ -1,11 +1,19 @@
-const RoadItem:React.FC = () => {
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-
-    return(
-    <li>
-
-    </li>)
+interface props {
+  data: { title: string; desc: string };
 }
 
+const RoadItem: React.FC<props> = ({ data }) => {
+  return (
+    <li className="w-full flex justify-start items-start gap-2">
+      <span className="text-xl">
+        <FontAwesomeIcon icon={faAngleRight} />
+      </span>
+      <p>{data.title}</p>
+    </li>
+  );
+};
 
 export default RoadItem;
