6198cf2e3388d268fb3457aed90d64b824e09b7d
aly3n <aly3n.dev@gmail.com>
2022-04-14 03:19:45 +0430

    next config bug fixed

--- next.config.js
+++ next.config.js
@@ -13,7 +13,6 @@ module.exports = {
   //     },
   //   ];
   // },
-<<<<<<< HEAD
   // async redirects() {
   //   return [
   //     {
@@ -33,25 +32,4 @@ module.exports = {
   //     },
   //   ];
   // },
-=======
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/underconstruction",
-        permanent: true,
-      },
-      {
-        source: "/gallery",
-        destination: "/underconstruction",
-        permanent: true,
-      },
-      {
-        source: "/gameboard",
-        destination: "/underconstruction",
-        permanent: true,
-      },
-    ];
-  },
->>>>>>> d0b16ffdd53004cb4d763d6aba07a3ccf871d12a
 };
