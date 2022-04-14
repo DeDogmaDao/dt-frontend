3d20c1905c822d82716de1af515afdb3d13ab2d4
aly3n <aly3n.dev@gmail.com>
2022-04-14 17:29:10 +0430

    feat(benefit): benefit click handler completed

--- src/components/howItWorksPage/benefits/Benefits.tsx
+++ src/components/howItWorksPage/benefits/Benefits.tsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
 import { benefitsData } from "../../../store/allData";
 import EachBenefit from "./EachBenefit";
 
 const Benefits: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
   return (
     <div className="flex flex-col justify-center items-center my-20">
       <div className="flex justify-center items-center gap-x-12 text-extera-large-medium">
         {benefitsData.map((data, index) => {
-          return <EachBenefit data={data} index={index} />;
+          return (
+            <EachBenefit
+              data={data}
+              index={index}
+              setActiveIndex={setActiveIndex}
+              activeIndex={activeIndex}
+            />
+          );
         })}
       </div>
-      <div className="w-9/12 h-28 mt-16 flex justify-center items-center bg-red-300"></div>
+      <div className="w-9/12 h-28 mt-16 flex justify-center items-center bg-red-300">
+        {benefitsData[activeIndex].desc.map(item=>{
+          return <div>{item}</div>
+        })}
+      </div>
     </div>
   );
 };
--- src/components/howItWorksPage/benefits/EachBenefit.tsx
+++ src/components/howItWorksPage/benefits/EachBenefit.tsx
@@ -1,12 +1,35 @@
+import { Dispatch, SetStateAction, useState } from "react";
 import { benefitType } from "../../../types/allTypes";
 
 interface props {
     data:benefitType;
     index:number;
+    setActiveIndex: Dispatch<SetStateAction<number>>;
+    activeIndex: number;
 }
-const EachBenefit: React.FC<props> = ({data,index}) => {
+const EachBenefit: React.FC<props> = ({data,index, activeIndex, setActiveIndex}) => {
+
+  const benefitBtnClickHandler = () => {
+    setActiveIndex((prevState) => {
+      if (prevState === index) {
+        return prevState;
+      }
+      return -1;
+    });
+    setTimeout(() => {
+        setActiveIndex(prevState=>{
+          if(prevState===-1){
+              return index
+          }
+          return prevState
+      });
+    }, 100);
+  }
+
   return (
-    <button className="flex justify-center items-center">
+    <button className="flex justify-center items-center"
+    onClick={benefitBtnClickHandler}
+    >
       <div>{data.title}</div>
       <div></div>
     </button>
