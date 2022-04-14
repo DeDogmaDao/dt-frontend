1e35e7fe160926f86d43cfc461f1023eee1198f1
aly3n <aly3n.dev@gmail.com>
2022-04-14 23:10:30 +0430

    feat(callToAction):callToAction completed

--- src/components/howItWorksPage/callToAction/ToRoadmapAction.tsx
+++ src/components/howItWorksPage/callToAction/ToRoadmapAction.tsx
@@ -1,11 +1,15 @@
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 import CallToActionSvg from "./CallToActionSvg";
 
 const ToRoadmapAction: React.FC = () => {
   return (
-    <div className="w-full h-full justify-center pb-96">
-      <div className="flex flex-col justify-start items-center">
+    <div className="w-full h-screen justify-center relative">
+      <div className=" z-0 w-full h-full">
+        <Image src={"/img/bg/bg2.png"} layout="fill" />
+      </div>
+      <div className="flex flex-col justify-start items-center z-10 absolute top-0 left-0 w-full h-full">
         <span className="w-7 h-1 mt-36 bg-yellow-300" />
         <h3 className="mt-8">
           Our <span className="text-yellow-300">Roadmap</span>
@@ -15,7 +19,7 @@ const ToRoadmapAction: React.FC = () => {
           accomplish and what features your product will include
         </p>
         <div className="relative w-full flex justify-center items-center">
-          <div className="w-full absolute top-0 left-[25%]">
+          <div className="absolute top-0 left-[25%]">
             <CallToActionSvg />
           </div>
           <button className="px-4 py-3 mt-11 bg-black rounded-full text-small-bold flex gap-x-2 duration-500 border-2 border-transparent hover:border-yellow-300">
