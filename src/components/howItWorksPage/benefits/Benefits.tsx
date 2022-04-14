2e351c8e7f85b841bb71f9bab11bacb94e7ea5c1
aly3n <aly3n.dev@gmail.com>
2022-04-14 16:53:11 +0430

    feat(benefits): EachBenefit completed

--- src/components/howItWorksPage/benefits/Benefits.tsx
+++ src/components/howItWorksPage/benefits/Benefits.tsx
@@ -1,19 +1,17 @@
 import { benefitsData } from "../../../store/allData";
+import EachBenefit from "./EachBenefit";
 
-const Benefits:React.FC = () => {
-
-    return(
-        <div className="flex flex-col justify-center items-center">
-            <div className="flex justify-center items-center">
-{benefitsData.map(data=>{
-    return
-})}
-            </div>
-            <div className="w-9/12 h-28 flex justify-center items-center bg-red-300">
-
-            </div>
-        </div>
-    )
-}
+const Benefits: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center my-20">
+      <div className="flex justify-center items-center gap-x-12 text-extera-large-medium">
+        {benefitsData.map((data, index) => {
+          return <EachBenefit data={data} index={index} />;
+        })}
+      </div>
+      <div className="w-9/12 h-28 mt-16 flex justify-center items-center bg-red-300"></div>
+    </div>
+  );
+};
 
 export default Benefits;
--- src/components/howItWorksPage/benefits/EachBenefit.tsx
+++ src/components/howItWorksPage/benefits/EachBenefit.tsx
@@ -1,7 +1,13 @@
-const EachBenefit: React.FC = () => {
+import { benefitType } from "../../../types/allTypes";
+
+interface props {
+    data:benefitType;
+    index:number;
+}
+const EachBenefit: React.FC<props> = ({data,index}) => {
   return (
-    <button>
-      <div></div>
+    <button className="flex justify-center items-center">
+      <div>{data.title}</div>
       <div></div>
     </button>
   );
