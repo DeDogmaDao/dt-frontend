5bc568dde828be9c44132284235ed5cfbef0e1b0
aly3n <aly3n.dev@gmail.com>
2022-04-16 03:39:12 +0430

    feat(hero): images added

--- src/components/howItWorksPage/HowItWorks.tsx
+++ src/components/howItWorksPage/HowItWorks.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { pageAnimation } from "../../utils/animation";
 import Benefits from "./benefits/Benefits";
 import ToRoadmapAction from "./callToAction/ToRoadmapAction";
+import Hero from "./hero/Hero";
 import Tabs from "./Tabs";
 
 const HowItWorks: React.FC = () => {
@@ -12,12 +13,7 @@ const HowItWorks: React.FC = () => {
       exit="out"
       variants={pageAnimation}
     >
-      {/* <HeaderImg />
-
-        <SecOne />
-        <Carosel allData={teamClone} Item={Card} initialQuantity={100} />
-        <FAQ />
-        <Tabs /> */}
+      <Hero />
       <Benefits />
       <Tabs />
       <ToRoadmapAction />
--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 const Hero:React.FC = () => {
 
 return (
-    <div></div>
+    <div className="flex justify-center items-center relative w-screen h-[76.4vw] overflow-hidden hero-container">
+        <div className="portal-and-flame-and-hero w-full h-full relative z-10">
+            <span className="w-full h-full max-w-none"><Image src={'/img/art/portal.png'} layout="fill" /></span>
+        </div>
+        <span className="absolute w-full h-full z-0"><Image src={'/img/art/city.png'} layout="fill" /></span>
+    </div>
 );
 }
 
