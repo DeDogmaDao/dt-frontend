959be04a9694784635be275690e9beab22c1801d
aly3n <aly3n.dev@gmail.com>
2022-04-15 18:22:19 +0430

    feat(layout): memorizing render conditions

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -3,21 +3,29 @@ import Header from "./Header";
 import Main from "./Main";
 import { useRouter } from "next/router";
 import { AnimatePresence } from "framer-motion";
+import { useMemo } from "react";
 const Layout: React.FC = (props) => {
   const router = useRouter();
   const gameBoardCondition = router.asPath === "/gameboard";
   const underConstructionCondition = router.asPath === "/underconstruction";
   const roadmapCondition = router.asPath === "/roadmap";
+
+  const layoutCondition = useMemo(() => {
+    return [
+      !gameBoardCondition && !underConstructionCondition,
+      !gameBoardCondition && !underConstructionCondition && !roadmapCondition,
+    ];
+  }, [router.asPath]);
   return (
     <div
       id="__layout"
       className="w-full h-full  flex flex-col justify-between items-start overflow-hidden relative"
     >
-      {!gameBoardCondition && !underConstructionCondition && <Header />}
+      {layoutCondition[0] && <Header />}
       <AnimatePresence exitBeforeEnter>
-      <Main key={router.asPath} >{props.children}</Main>
+        <Main key={router.asPath}>{props.children}</Main>
       </AnimatePresence>
-      {!gameBoardCondition && !underConstructionCondition && !roadmapCondition && <Footer />}
+      {layoutCondition[1] && <Footer />}
     </div>
   );
 };
