ddb33c72ecfa73aff5a8cf1b1f4365c9a17c1d75
aly3n <aly3n.dev@gmail.com>
2022-04-16 00:11:04 +0430

    feat(faq): all faq completed

--- src/components/faq/EachFAQ.tsx
+++ src/components/faq/EachFAQ.tsx
@@ -12,25 +12,24 @@ const EachFAQ: React.FC<props> = ({ data }) => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   return (
-    <motion.li
-      layout
-      className="w-11/12 group z-10 rounded-lg overflow-hidden "
-    >
-      <motion.div layout className="w-full h-full flex flex-col justify-center items-center z-10  hover:bg-neutral-700 ">
+    <motion.li layout className="w-11/12 group z-10 rounded-lg overflow-hidden shadow-md hover:shadow-inner">
+      <div
+        className="w-full h-full flex flex-col justify-center items-center z-10  hover:bg-neutral-700 duration-500"
+      >
         <motion.div
           layout
           className="w-full flex justify-between items-center px-5 !h-14 cursor-pointer text-medium-medium "
           onClick={() => setIsExpanded((prev) => !prev)}
         >
-          <div className="flex gap-x-2">
-            <span className="w-px h-5 bg-yellow-300" />
+          <div className="flex gap-x-2 items-center">
+            <span className="w-px h-2 bg-yellow-300 duration-500 group-hover:h-6 group-hover:w-px" />
             <span>
               <FontAwesomeIcon icon={faMagnifyingGlass} />
             </span>
             <span>{data.question}</span>
           </div>
           <span
-            className={`bg-secondary-500 text-white rounded-full !h-16 !w-16 !min-w-[64px] ml-3 flex justify-center items-center  group-hover:rotate-90 group-hover:bg-secondary-400
+            className={`bg-secondary-500 text-white rounded-full !h-16 !w-16 !min-w-[64px] ml-3 flex justify-center items-center  group-hover:rotate-90 group-hover:bg-secondary-400 duration-500
           ${isExpanded ? "!rotate-45" : ""}`}
           >
             <FontAwesomeIcon icon={faPlus} />
@@ -45,7 +44,7 @@ const EachFAQ: React.FC<props> = ({ data }) => {
           </motion.p>
         )}
         <motion.div layout className="h-px w-10/12 bg-disabled"></motion.div>
-      </motion.div>
+      </div>
     </motion.li>
   );
 };
