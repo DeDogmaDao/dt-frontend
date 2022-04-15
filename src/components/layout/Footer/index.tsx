666139e9fa3c0c2ffbd4c0861a3f042115ffab3e
aly3n <aly3n.dev@gmail.com>
2022-04-15 19:43:25 +0430

    feat(footer): background gradient added to footer

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -4,16 +4,29 @@ import Social from "./Social";
 const Footer: React.FC = () => {
   return (
     <>
-      <footer className="h-full w-full flex flex-col justify-center items-center z-100 bg-neutral-900">
-        <div className="-mt-14 mb-3"><Image src="/img/logo/treasure.png" width={284} height={94} /></div>
-        <h3 className="mb-3" >NFT Game and Collection by <span className="text-yellow-300">DDD</span></h3>
+      <footer
+        className="h-full w-full flex flex-col justify-center items-center z-100 -mt-32 "
+        style={{
+          background:
+            "linear-gradient(0deg, rgba(33,33,33,1) 0%, rgba(33,33,33,1) 80%, rgba(0,0,0,0) 100%)",
+        }}
+      >
+        <div className="pt-14 mb-3">
+          <Image src="/img/logo/treasure.png" width={284} height={94} />
+        </div>
+        <h3 className="mb-3">
+          NFT Game and Collection by{" "}
+          <span className="text-yellow-300">DDD</span>
+        </h3>
         <span className="w-[30px] h-[6px] mb-16 bg-yellow-300" />
         <ul className="flex justify-center items-center gap-x-3 mb-36">
           {footerData.map((data) => {
             return <Social data={data} />;
           })}
         </ul>
-        <p className="mb-40 text-large-light">&copy; COPYWRITES 2022 DDD. ALL RIGHTS RESERVED</p>
+        <p className="mb-40 text-large-light">
+          &copy; COPYWRITES 2022 DDD. ALL RIGHTS RESERVED
+        </p>
       </footer>
     </>
   );
