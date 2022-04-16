3c4d4819721b11915bab73496126803fb30d8a5f
aly3n <aly3n.dev@gmail.com>
2022-04-16 04:47:32 +0430

    scales fixed

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 
 const Hero: React.FC = () => {
   const [innerWidth, setInnerWidth] = useState(0);
-  const [scaleValue,setScaleValue] = useState(1)
+  const [scaleValue, setScaleValue] = useState(1);
   useEffect(() => {
     setInnerWidth(window.innerWidth);
     setTimeout(() => {
-        setScaleValue(1.3)
+      setScaleValue(1.3);
     }, 3000);
   }, []);
 
@@ -16,23 +16,21 @@ const Hero: React.FC = () => {
     <div className="flex justify-center items-center relative w-[100vw] h-[76.4vw] mx-auto overflow-hidden hero-container">
       <div className="portal-and-flame-and-hero w-full h-full relative z-10">
         <motion.span
-          className="absolute duration-500"
+          className="absolute duration-500 opacity-90"
           style={{
             marginLeft: (-innerWidth * (scaleValue - 1)) / 2,
             marginTop: (-innerWidth * 0.7636 * (scaleValue - 1)) / 2,
-            width:innerWidth * scaleValue,
-            height:0.7636 * innerWidth * scaleValue
+            width: innerWidth * scaleValue,
+            height: 0.7636 * innerWidth * scaleValue,
           }}
         >
-          <Image
-            src={"/img/art/portal.png"}
-            layout="fill"
-
-          />
+          <Image src={"/img/art/portal.png"} layout="fill" />
         </motion.span>
       </div>
-      <span className="absolute w-full h-full z-0"
-      style={{marginTop:(-innerWidth * 0.7636 * 0.3) / 2,}}>
+      <span
+        className="absolute w-full h-full z-0"
+        style={{ marginTop: (-innerWidth * 1.5 * 0.3) / 2 }}
+      >
         <Image
           src={"/img/art/city.png"}
           layout="fixed"
