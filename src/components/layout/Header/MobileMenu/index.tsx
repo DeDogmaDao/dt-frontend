b586591846a098b349b46e68ede839335ca11177
aly3n <aly3n.dev@gmail.com>
2022-04-17 00:31:07 +0430

    feat(mobile menu): open close animation added

--- src/components/layout/Header/MobileMenu/index.tsx
+++ src/components/layout/Header/MobileMenu/index.tsx
@@ -1,18 +1,24 @@
 import { motion, useCycle } from "framer-motion";
 import { useRef } from "react";
+import { useDimensions } from "../../../../hooks/useDimensions";
 
 const MobileMenu: React.FC = () => {
   const mobileMenuContainerRef = useRef(null);
   const [isExpanded, cycleIsExpanded] = useCycle(false, true);
+  const dimension = useDimensions(mobileMenuContainerRef);
   return (
     <motion.nav
       initial={false}
       animate={isExpanded ? "opened" : "closed"}
       ref={mobileMenuContainerRef}
+      custom={dimension.height}
       className="absolute top-0 right-0 bottom-0 w-80"
     >
-      <motion.div className="w-screen absolute top-0 right-0 bottom-0">
-          
+      <motion.div
+      
+
+      className="w-screen absolute top-0 right-0 bottom-0">
+
       </motion.div>
     </motion.nav>
   );
--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -760,3 +760,23 @@ export const callToactionSvgArrowRightAni: Variants = {
     },
   },
 };
+
+export const mobileMenuAni: Variants = {
+  opened: (custom = 1000) => ({
+    clipPath: `circle(${custom * 2 + 200}px at 40px 40px)`,
+    transition: {
+      type: "spring",
+      stiffness: 20,
+      restDelta: 2,
+    },
+  }),
+  closed: {
+    clipPath: "circle(30px at 40px 40px)",
+    transition: {
+      delay: 0.5,
+      type: "spring",
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+};
