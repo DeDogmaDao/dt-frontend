760bace4139a052a5e263b8a5a3a07262fe1dfc0
aly3n <aly3n.dev@gmail.com>
2022-04-18 02:03:11 +0430

    bug fixed

--- src/components/roadMapPage/ProgressLine.tsx
+++ src/components/roadMapPage/ProgressLine.tsx
@@ -7,7 +7,7 @@ interface props {
 
 const ProgressLine: React.FC<props> = ({activeSection}) => {
   return (
-    <div className="absolute top-0 left-1/2 ml-[-8px] flex flex-col items-center gap-y-5 z-100 mt-20">
+    <div className="absolute top-0 left-1/2 ml-[-8px] w-1 flex flex-col items-center gap-y-5 z-100 mt-20">
       <div className="text-3xl text-primary-500"><FontAwesomeIcon icon={faEthereum} /></div>
       <div className="w-1 h-[500px] bg-neutral-400 overflow-hidden rounded-full relative">
         <div
