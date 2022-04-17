c4aee7699167e9ae57e07d0081cd48082d26146c
aly3n <aly3n.dev@gmail.com>
2022-04-17 15:49:31 +0430

    hero completed

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -1,4 +1,9 @@
-import { motion, useSpring, useTransform, useViewportScroll } from "framer-motion";
+import {
+  motion,
+  useSpring,
+  useTransform,
+  useViewportScroll,
+} from "framer-motion";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 
@@ -8,9 +13,8 @@ const Hero: React.FC = () => {
   const [innerWidth, setInnerWidth] = useState(0);
   useEffect(() => {
     setInnerWidth(window.innerWidth);
-
   }, []);
-  const { scrollY,scrollYProgress } = useViewportScroll();
+  const { scrollY, scrollYProgress } = useViewportScroll();
   const marginLeftT = useTransform(
     scrollY,
     (y) => (-innerWidth * (1 + y / speed - 1)) / 2
@@ -32,19 +36,21 @@ const Hero: React.FC = () => {
     scrollY,
     (y) => innerWidth * 0.4 * (1 + y / speed)
   );
-  const scaleY = useTransform(scrollYProgress,y=>1 -y)
-  
-  const marginLeft = useSpring(marginLeftT,{damping:20})
-  const marginTop = useSpring(marginTopT,{damping:20})
-  const width = useSpring(widthT,{damping:20})
-  const height = useSpring(heightT,{damping:20})
-  const widthGif = useSpring(widthGifT,{damping:20})
-  const bottomGif = useSpring(bottomGifT,{damping:20})
-  const scale = useSpring(scaleY,{damping:20})
+  const scaleT = useTransform(scrollYProgress, (y) => 1 - y);
+  const opacityT = useTransform(scrollYProgress, (y) => 0.8 - y * 3);
+
+  const marginLeft = useSpring(marginLeftT, { damping: 20 });
+  const marginTop = useSpring(marginTopT, { damping: 20 });
+  const width = useSpring(widthT, { damping: 20 });
+  const height = useSpring(heightT, { damping: 20 });
+  const widthGif = useSpring(widthGifT, { damping: 20 });
+  const bottomGif = useSpring(bottomGifT, { damping: 20 });
+  const scale = useSpring(scaleT, { damping: 20 });
+  const opacity = useSpring(opacityT, { damping: 20 });
 
   return (
     <div className="flex justify-center items-center relative w-[100vw] h-[56.26vw] mx-auto overflow-hidden hero-container">
-      <div className="portal-and-flame-and-hero w-full h-full relative z-10 ">
+      <div className="portal-and-flame-and-hero w-full h-full relative z-20 ">
         <motion.span
           className="absolute  "
           style={{
@@ -66,9 +72,19 @@ const Hero: React.FC = () => {
         </motion.span>
       </div>
       <motion.span
+        className="absolute w-full h-full z-10"
+        style={{ marginTop: (-innerWidth * 1 * 0.3) / 2, scale, opacity }}
+      >
+        <Image
+          src={"/img/art/mask.png"}
+          layout="fixed"
+          width={innerWidth}
+          height={0.5626 * innerWidth * 1.3}
+        />
+      </motion.span>
+      <motion.span
         className="absolute w-full h-full z-0"
-        style={{ marginTop: (-innerWidth * 1 * 0.3) / 2,
-      scale }}
+        style={{ marginTop: (-innerWidth * 1 * 0.3) / 2, scale }}
       >
         <Image
           src={"/img/art/city.png"}
