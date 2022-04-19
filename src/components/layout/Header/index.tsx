d4e0d0224ebb17cfb3ff106c2f3f408bd04d97af
aly3n <aly3n.dev@gmail.com>
2022-04-19 06:18:40 +0430

    feat(navbar): desktop navbar

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -46,22 +46,22 @@ const Header: React.FC = () => {
     <div className="absolute top-0 left-0 w-full h-[84px] z-[10000] ">
       <div className="flex justify-start items-center w-full h-full relative pt-9">
         <div className="absolute top-0 left-0 w-full h-[160px] bg-gradient-to-b from-neutral-900 via-neutral-900/70 to-transparent " />
-        <span className="absolute top-[35px] left-6 z-100 ">
+        <span className="absolute top-[35px] left-16 z-100 ">
           <Link href="/">
             <div className="flex justify-start items-center gap-x-2">
               <img
                 className="w-9 cursor-pointer"
                 src="/img/logo/ddd-logo.png"
               />
-              <span className="text-medium-bold hidden md:block">Demmortal Treasure</span>
+              <span className="text-medium-bold md:hidden">Demmortal Treasure</span>
             </div>
           </Link>
         </span>
-        {/* <motion.ul className="flex justify-start items-center ml-9 gap-x-8">
+        <motion.ul className="absolute top-[35px] left-[154px] flex justify-start items-center gap-x-8 z-110">
           {headerLinks.map((headLink) => {
             return <EachLink key={headLink.ref} headLink={headLink} />;
           })}
-        </motion.ul> */}
+        </motion.ul>
         <span className="absolute top-[32px] right-[72px] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
           <FontAwesomeIcon icon={faDiscord} />
         </span>
