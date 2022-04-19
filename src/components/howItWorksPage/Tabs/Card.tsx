965ed6f62608c92b456b014cde5d07ddeb74578e
aly3n <aly3n.dev@gmail.com>
2022-04-19 22:53:44 +0430

    cards changed

--- src/components/howItWorksPage/Tabs/Card.tsx
+++ src/components/howItWorksPage/Tabs/Card.tsx
@@ -35,7 +35,7 @@ const Card: React.FC<props> = ({
   const cardPlace = cardPlaceDetector(activeIndexCard[tabGroup], index);
   let style: MotionStyle = {
     top: 0,
-    marginLeft: "-100px",
+    marginLeft: "-150px",
   };
 
   return (
@@ -48,7 +48,7 @@ const Card: React.FC<props> = ({
       variants={carouselDisplayAni}
       custom={cardPlace}
       onClick={(e) => cardClickHandler(e)}
-      className={`h-[420px] w-[200px] flex  text-white absolute select-none hover:cursor-pointer  ${
+      className={`h-[424px] w-[300px] flex  text-white absolute select-none hover:cursor-pointer  ${
         activeIndexCard[tabGroup] === index ? "grayscale-0" : "grayscale"
       }`}
     >
@@ -56,8 +56,8 @@ const Card: React.FC<props> = ({
         draggable={false}
         className="object-contain"
         src={data.image}
-        height="420"
-        width="200"
+        height="424"
+        width="300"
       />
     </motion.div>
   );
