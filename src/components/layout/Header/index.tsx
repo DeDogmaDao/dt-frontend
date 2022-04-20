70e10c613e4d34524ba7331d571698477ce688a6
aly3n <aly3n.dev@gmail.com>
2022-04-20 06:31:06 +0430

    responsive for wide screens finished

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -46,7 +46,7 @@ const Header: React.FC = () => {
     <div className="absolute top-0 left-0 w-full h-[5.25rem] z-[10000] pointer-events-none">
       <div className="flex justify-start items-center w-full h-full relative">
         <div className="absolute top-0 left-0 w-full h-[10rem] bg-gradient-to-b from-[#000413] via-[#000413]/30 to-transparent " />
-        <span className="absolute top-[2.1875rem] mt-[-.625rem] left-5 ssm:left-16 z-100 pointer-events-auto">
+        <span className="absolute top-[2.1875rem]  left-5 ssm:left-16 z-100 pointer-events-auto">
           <Link href="/">
             <div className="flex justify-start items-center gap-x-2">
               <img
@@ -59,12 +59,12 @@ const Header: React.FC = () => {
             </div>
           </Link>
         </span>
-        <motion.ul className="absolute pointer-events-auto top-[2.1875rem] left-[7.125rem] lg:left-[9.625rem] hidden md:flex justify-start items-center gap-x-5 lg:gap-x-8 z-110">
+        <motion.ul className="absolute pointer-events-auto top-[2.565rem] mt-[-.3rem] 2xl:mt-0 left-[7.125rem] lg:left-[9.625rem] hidden md:flex justify-start items-center gap-x-5 lg:gap-x-8 z-110">
           {headerLinks.map((headLink) => {
             return <EachLink key={headLink.ref} headLink={headLink} />;
           })}
         </motion.ul>
-        <span className="absolute pointer-events-auto top-[2rem] mt-[-.625rem] right-[4.5rem] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
+        <span className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
           <FontAwesomeIcon icon={faDiscord} />
         </span>
         <MobileMenu headerLinks={headerLinks} />
