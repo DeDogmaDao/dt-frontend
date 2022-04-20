e464823a33c1ad4873252b6bff665694d3356575
aly3n <aly3n.dev@gmail.com>
2022-04-20 05:49:15 +0430

    header px to rem

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -43,10 +43,10 @@ const Header: React.FC = () => {
   ];
 
   return (
-    <div className="absolute top-0 left-0 w-full h-[84px] z-[10000] pointer-events-none">
+    <div className="absolute top-0 left-0 w-full h-[5.25rem] z-[10000] pointer-events-none">
       <div className="flex justify-start items-center w-full h-full relative">
-        <div className="absolute top-0 left-0 w-full h-[160px] bg-gradient-to-b from-[#000413] via-[#000413]/30 to-transparent " />
-        <span className="absolute top-[35px] left-5 ssm:left-16 z-100 pointer-events-auto">
+        <div className="absolute top-0 left-0 w-full h-[10rem] bg-gradient-to-b from-[#000413] via-[#000413]/30 to-transparent " />
+        <span className="absolute top-[2.1875rem] left-5 ssm:left-16 z-100 pointer-events-auto">
           <Link href="/">
             <div className="flex justify-start items-center gap-x-2">
               <img
@@ -59,12 +59,12 @@ const Header: React.FC = () => {
             </div>
           </Link>
         </span>
-        <motion.ul className="absolute pointer-events-auto top-[35px] left-[114px] lg:left-[154px] hidden md:flex justify-start items-center gap-x-5 lg:gap-x-8 z-110">
+        <motion.ul className="absolute pointer-events-auto top-[2.1875rem] left-[7.125rem] lg:left-[9.625rem] hidden md:flex justify-start items-center gap-x-5 lg:gap-x-8 z-110">
           {headerLinks.map((headLink) => {
             return <EachLink key={headLink.ref} headLink={headLink} />;
           })}
         </motion.ul>
-        <span className="absolute pointer-events-auto top-[32px] right-[72px] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
+        <span className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
           <FontAwesomeIcon icon={faDiscord} />
         </span>
         <MobileMenu headerLinks={headerLinks} />
