aef72aed269cfcfbdf739978d09c401a2c5531bb
aly3n <aly3n.dev@gmail.com>
2022-04-22 02:39:39 +0430

    refactor(new design):dis btn hover

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -60,14 +60,15 @@ const Header: React.FC = () => {
         </motion.ul>
         <button
           className=" pointer-events-auto absolute top-[2rem] right-[7.7rem] w-[8.5rem] h-10 rounded-full bg-neutral-600 text-white text-sm font-bold 
-    flex justify-evenly items-center"
+    flex justify-evenly items-center hover:bg-neutral-500 duration-200"
         >
           <span className=" ">
             <GitBookSVG />
           </span>
           <span>LightBook</span>
         </button>
-        <button className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600 text-white text-xl flex justify-center items-center">
+        <button className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600
+         text-white text-xl flex justify-center items-center hover:bg-neutral-500 duration-200">
           <DiscordSVG />
         </button>
         <MobileMenu headerLinks={headerLinks} />
