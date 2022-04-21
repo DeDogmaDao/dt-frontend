f2f778ce1fe1fd1883e764edd4f2adab28921918
aly3n <aly3n.dev@gmail.com>
2022-04-21 06:16:53 +0430

    feat(hero-v2): hero v2 completed

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -14,7 +14,6 @@ import { dimensionType } from "../../../types/allTypes";
 
 const Hero: React.FC = () => {
   const heroContainerRef = useRef<HTMLDivElement>(null);
-  const capeRef = useRef<>(null);
   const [PortalLoading, setPortalLoading] = useState(0);
   const [dimension, setDimension] = useState<dimensionType>({
     width: 0,
@@ -64,10 +63,10 @@ const Hero: React.FC = () => {
     }
   }, []);
 
-  useEffect(()=>{
+  useEffect(() => {
     // @ts-ignore
-    capeRef.current!.stop()
-  },[])
+    capeRef.current!.stop();
+  }, []);
 
   return (
     <>
@@ -119,7 +118,6 @@ const Hero: React.FC = () => {
               </motion.span>
 
               <motion.img
-              ref={capeRef}
                 className="absolute z-20 left-[48.5%] top-[71%] w-[10%] h-[15%] will-change-transform"
                 src={"/img/art/cape.gif"}
               />
