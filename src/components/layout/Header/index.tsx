2b8554c827fa3cc2bce03bf8de92b944a51938f6
aly3n <aly3n.dev@gmail.com>
2022-04-22 02:32:35 +0430

    refactor(new design):header back color

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -37,17 +37,9 @@ const Header: React.FC = () => {
   ];
 
   return (
-    <div className=" w-full h-[5.25rem] z-[10000] pointer-events-none">
+    <div className="absolute top-0 left-0 w-full h-[5.25rem] z-[10000] pointer-events-none">
       <div className="flex justify-start items-center w-full h-full relative">
-        <div
-          className="absolute top-0 left-0 w-full h-[7.5rem] "
-          style={{
-            background:
-              "linear-gradient(180deg, #15161C 0%, #0B0C18 39.92%, rgba(20, 20, 28, 0) 100%)",
-            filter: "blur(70px)",
-            backdropFilter: "blur(138px)",
-          }}
-        />
+      <div className="absolute top-0 left-0 w-full h-[7.5rem] bg-gradient-to-b from-[#090919] via-[#090919]/50 to-transparent " />
         <span className="absolute top-[2.1875rem]  left-5 ssm:left-16 z-100 pointer-events-auto">
           <Link href="/">
             <div className="flex justify-start items-center gap-x-2">
@@ -67,7 +59,7 @@ const Header: React.FC = () => {
           })}
         </motion.ul>
         <button
-          className=" pointer-events-auto absolute top-[2rem] right-[7.7rem] w-[8.5rem] h-10 rounded-full bg-neutral-700 text-white text-sm font-bold 
+          className=" pointer-events-auto absolute top-[2rem] right-[7.7rem] w-[8.5rem] h-10 rounded-full bg-neutral-600 text-white text-sm font-bold 
     flex justify-evenly items-center"
         >
           <span className=" ">
@@ -75,7 +67,7 @@ const Header: React.FC = () => {
           </span>
           <span>LightBook</span>
         </button>
-        <button className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
+        <button className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600 text-white text-xl flex justify-center items-center">
           <DiscordSVG />
         </button>
         <MobileMenu headerLinks={headerLinks} />
