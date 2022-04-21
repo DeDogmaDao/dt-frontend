12cc191ace39613dcb2088ddc9e0373c0e468a2d
aly3n <aly3n.dev@gmail.com>
2022-04-21 05:31:33 +0430

    feat(hero-v2): will changed added to all transforming element

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -73,7 +73,7 @@ const Hero: React.FC = () => {
       >
         <div className="portal-and-flame-and-hero w-full h-full relative z-20 ">
           <motion.span
-            className="absolute w-full h-full origin-bottom"
+            className="absolute w-full h-full origin-bottom will-change-transform"
             initial="hidden"
             animate={anim}
             variants={heroAnim}
@@ -82,23 +82,25 @@ const Hero: React.FC = () => {
               className=" ml-[-100%] sm:ml-[-25%] lg:ml-auto
              h-full
              w-[300%] sm:w-[150%] lg:w-full
-             relative flex justify-center items-center"
+             relative flex justify-center items-center will-change-transform"
             >
               <motion.div
                 onHoverStart={() => setAnim("visible")}
                 onHoverEnd={() => setAnim("hidden")}
-                className="red-transparent-on-city absolute w-[20%] h-[65%] top-1/2 mt-[-19.5%] left-1/2 ml-[-10%]  z-100 rounded-[50%]"
+                className="red-transparent-on-city absolute w-[20%] h-[65%] top-1/2 mt-[-19.5%] left-1/2 ml-[-10%]  z-100 rounded-[50%] will-change-transform"
               />
-              <motion.span className="mt-[10%] z-20 absolute">
+              <motion.span className="mt-[10%] z-20 absolute will-change-transform">
                 <Image src="/img/logo/treasure.png" width={231} height={70} />
               </motion.span>
               <motion.button
                 className="text-small-bold absolute rounded-full w-[138px] h-[40px] flex justify-center items-center text-white
-              bg-neutral-900 z-20 mt-[17%]"
+              bg-neutral-900 z-110 mt-[17%] cursor-pointer will-change-transform"
+                onHoverStart={() => setAnim("visible")}
+                onHoverEnd={() => setAnim("hidden")}
               >
                 Join Our Discord
               </motion.button>
-              <motion.span className="absolute w-full h-full z-10">
+              <motion.span className="absolute w-full h-full z-10 will-change-transform">
                 <Image
                   onLoadingComplete={() =>
                     setPortalLoading((prevState) => prevState + 1)
@@ -111,7 +113,7 @@ const Hero: React.FC = () => {
               </motion.span>
 
               <motion.img
-                className="absolute z-20 left-[48.5%] top-[71%] w-[10%] h-[15%]"
+                className="absolute z-20 left-[48.5%] top-[71%] w-[10%] h-[15%] will-change-transform"
                 src={"/img/art/cape.gif"}
               />
               <motion.span
@@ -120,7 +122,7 @@ const Hero: React.FC = () => {
                   y: springY,
                   visibility: PortalLoading > 0 ? "visible" : "hidden",
                 }}
-                className="absolute w-full h-full z-0"
+                className="absolute w-full h-full z-0 will-change-transform"
                 initial="hidden"
                 animate={anim}
                 variants={cityAnim}
@@ -132,7 +134,7 @@ const Hero: React.FC = () => {
                   opacity: maskOpacity,
                   visibility: PortalLoading > 0 ? "visible" : "hidden",
                 }}
-                className="absolute w-full h-full z-0 scale-105"
+                className="absolute w-full h-full z-0 scale-105 will-change-transform"
               >
                 <Image src={"/img/art/mask.png"} layout="fill" />
               </motion.span>
@@ -144,7 +146,7 @@ const Hero: React.FC = () => {
                 autoPlay
                 muted
                 playsInline
-                className="absolute border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[14.576%] top-[57.828%]"
+                className="absolute border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[14.576%] top-[57.828%] will-change-transform"
               >
                 <source src={"/img/art/left-fire.mp4"} type="video/mp4" />
               </motion.video>
@@ -155,7 +157,7 @@ const Hero: React.FC = () => {
                 autoPlay
                 muted
                 playsInline
-                className="absolute border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[74.844%] top-[57.828%]"
+                className="absolute border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[74.844%] top-[57.828%] will-change-transform"
               >
                 <source src={"/img/art/right-fire.mp4"} type="video/mp4" />
               </motion.video>
