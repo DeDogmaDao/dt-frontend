04b3de965a8609d69d27cf7866a300a9fbdc7d78
aly3n <aly3n.dev@gmail.com>
2022-04-22 02:02:08 +0430

    svgr added

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -9,6 +9,7 @@ import { LinkType } from "../../../types/allTypes";
 import MobileMenu from "./MobileMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import DiscordSVG from '../../svgs/discord.svg';
 
 const Header: React.FC = () => {
   const headerLinks: LinkType[] = [
@@ -65,7 +66,7 @@ const Header: React.FC = () => {
           })}
         </motion.ul>
         <span className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
-          <img src= alt="" />
+          <DiscordSVG />
         </span>
         <MobileMenu headerLinks={headerLinks} />
       </div>
