02fb270675d701945a9d1f2ca668f5d0521746fd
aly3n <aly3n.dev@gmail.com>
2022-04-22 02:13:19 +0430

    refactor(new design):header svg replaced

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -9,7 +9,8 @@ import { LinkType } from "../../../types/allTypes";
 import MobileMenu from "./MobileMenu";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDiscord } from "@fortawesome/free-brands-svg-icons";
-import DiscordSVG from '../../svgs/discord.svg';
+import DiscordSVG from "../../svgs/discord.svg";
+import GitBookSVG from "../../svgs/gitbook.svg";
 
 const Header: React.FC = () => {
   const headerLinks: LinkType[] = [
@@ -65,9 +66,18 @@ const Header: React.FC = () => {
             return <EachLink key={headLink.ref} headLink={headLink} />;
           })}
         </motion.ul>
-        <span className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-9 h-9 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
+        <button
+          className=" pointer-events-auto absolute top-[2rem] right-[7.7rem] w-[8.5rem] h-10 rounded-full bg-neutral-700 text-white text-sm font-bold 
+    flex justify-evenly items-center"
+        >
+          <span className=" ">
+            <GitBookSVG />
+          </span>
+          <span>LightBook</span>
+        </button>
+        <button className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-700 text-white text-xl flex justify-center items-center">
           <DiscordSVG />
-        </span>
+        </button>
         <MobileMenu headerLinks={headerLinks} />
       </div>
     </div>
