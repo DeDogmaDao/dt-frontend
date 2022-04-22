03ae8e428d373d6a82c19f74d1d4fee3adb3e97b
aly3n <aly3n.dev@gmail.com>
2022-04-22 09:19:14 +0430

    team background

--- src/components/teamPage/Team.tsx
+++ src/components/teamPage/Team.tsx
@@ -13,16 +13,15 @@ const Team: React.FC = () => {
       exit="out"
       className="w-screen min-h-screen relative pb-32"
     >
-      <div className="w-full h-full absolute top-0 left-0">
-        <Image
-          src={"/img/team/team-bg.png"}
-          width="1438"
-          height="1128"
-          layout="responsive"
-        />
-      </div>
+      <span className="absolute w-full aspect-[1440/1097]">
+        <span className="w-full h-full inner-image-no-max-width">
+          <Image src={"/img/bg/sec4.png"} layout="fill" quality={100} />
+        </span>
+      </span>
       <div className="relative w-full h-full flex flex-col justify-start items-center pt-24 md:pt-32 pb-10">
-        <h3 className="text-[1.25rem] ssm:text-[1.75rem] sm:text-[2.375rem] font-bold">Meet the Creators</h3>
+        <h3 className="text-[1.25rem] ssm:text-[1.75rem] sm:text-[2.375rem] font-bold">
+          Meet the Creators
+        </h3>
         <div className="flex flex-wrap justify-center items-center w-full gap-8 mt-20">
           {teamDataLvl1.map((member, index) => {
             return (
