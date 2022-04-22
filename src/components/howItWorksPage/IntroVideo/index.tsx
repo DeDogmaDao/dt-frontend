a152760df351468d28ff3ab8e976ead34d0ea2f0
aly3n <aly3n.dev@gmail.com>
2022-04-22 07:06:50 +0430

    feat(intro video): video play puase added

--- src/components/howItWorksPage/IntroVideo/index.tsx
+++ src/components/howItWorksPage/IntroVideo/index.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
-
+import PlaySVG from '../../svgs/play.svg';
+import PauseSVG from '../../svgs/pause.svg';
 const InteroVideo: React.FC = () => {
   return (
-    <div className="w-full h-[50.875rem] flex justify-center items-center relative">
-      <span className="w-full  absolute aspect-[1440/905]">
+    <div className="w-full aspect-video flex justify-center items-center relative">
+      <span className="w-full  absolute h-full lg:aspect-video">
         <span className="w-full h-full inner-image-no-max-width">
           <Image src={"/img/bg/sec2.png"} layout="fill" />
         </span>
       </span>
-      <div className="aspect-video w-[58.125rem] rounded-[30px] z-10 mt-[-10%]">
+      <div className="aspect-video w-[70%] rounded-[30px] z-10 mt-[-10%] flex justify-center items-center
+      relative">
+          <span>
+
+          </span>
         <video
-          controls
           poster="/img/bg/Story.png"
           className="aspect-video w-[58.125rem] rounded-[30px] z-10 object-cover"
         >
