66989152ba705582763e4cb62fd1f78e3c1ebb99
aly3n <aly3n.dev@gmail.com>
2022-04-22 04:35:53 +0430

    refactor(new design): svg of header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -39,7 +39,7 @@ const Header: React.FC = () => {
   return (
     <div className="absolute top-0 left-0 w-full h-[5.25rem] z-[10000] pointer-events-none">
       <div className="flex justify-start items-center w-full h-full relative">
-      <div className="absolute top-0 left-0 w-full h-[7.5rem] bg-gradient-to-b from-[#090919] via-[#090919]/50 to-transparent " />
+        <div className="absolute top-0 left-0 w-full h-[7.5rem] bg-gradient-to-b from-[#090919] via-[#090919]/50 to-transparent " />
         <span className="absolute top-[2.1875rem]  left-5 ssm:left-16 z-100 pointer-events-auto">
           <Link href="/">
             <div className="flex justify-start items-center gap-x-2">
@@ -60,16 +60,18 @@ const Header: React.FC = () => {
         </motion.ul>
         <button
           className=" pointer-events-auto absolute top-[2rem] right-[7.7rem] w-[8.5rem] h-10 rounded-full bg-neutral-600 text-white text-sm font-bold 
-    flex justify-evenly items-center hover:bg-neutral-500 duration-200"
+    flex justify-evenly items-center hover:bg-neutral-500 duration-200 fill-primary-500 hover:fill-primary-700"
         >
           <span className=" ">
-            <GitBookSVG />
+            <GitBookSVG width={26} />
           </span>
           <span>LightBook</span>
         </button>
-        <button className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600
-         text-white text-xl flex justify-center items-center hover:bg-neutral-500 duration-200">
-          <DiscordSVG />
+        <button
+          className="absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600
+         text-white text-xl flex justify-center items-center hover:bg-neutral-500 duration-200 fill-primary-500 hover:fill-primary-700"
+        >
+          <DiscordSVG width={26} />
         </button>
         <MobileMenu headerLinks={headerLinks} />
       </div>
