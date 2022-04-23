c3456e397f900084327199baa36d197daeb27676
aly3n <aly3n.dev@gmail.com>
2022-04-23 08:07:20 +0430

    refactor(calltoaction): portal injected

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -99,39 +99,7 @@ const Hero: React.FC = () => {
             />
             <LogoType setAnim={setAnim} portalLoading={portalLoading} />
 
-            <motion.span className="absolute left-0 w-[32.17669%] h-full hidden sm:block z-10 will-change-transform">
-              <Image
-                onLoadingComplete={() =>
-                  setPortalLoading((prevState) => prevState + 1)
-                }
-                src={"/img/art/left-portal.png"}
-                layout="fill"
-                priority={true}
-                quality={100}
-              />
-            </motion.span>
-            <motion.span className="absolute right-0 w-[32.23981%] h-full hidden sm:block z-10 will-change-transform">
-              <Image
-                onLoadingComplete={() =>
-                  setPortalLoading((prevState) => prevState + 1)
-                }
-                src={"/img/art/right-portal.png"}
-                layout="fill"
-                priority={true}
-                quality={100}
-              />
-            </motion.span>
-            <motion.span className="absolute left-[32.12669%] w-[35.70889%] h-full z-[11] will-change-transform">
-              <Image
-                onLoadingComplete={() =>
-                  setPortalLoading((prevState) => prevState + 1)
-                }
-                src={"/img/art/middle-portal.png"}
-                layout="fill"
-                priority={true}
-                quality={100}
-              />
-            </motion.span>
+            <></>
 
             <motion.img
               className="absolute z-20 left-[48.5%] top-[71%] w-[10%] h-[15%] will-change-transform"
---
+++ src/components/howItWorksPage/hero/Portal.tsx
@@ -0,0 +1,56 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { Dispatch, SetStateAction, useEffect } from "react";
+
+interface props {
+  setPortalLoading: Dispatch<SetStateAction<number>>;
+  portalLoading: number;
+}
+
+const Portal: React.FC<props> = ({ setPortalLoading, portalLoading }) => {
+  useEffect(() => {
+    if (portalLoading === 3) {
+      setPortalLoading(4);
+    }
+  }, [portalLoading]);
+
+  return (
+    <>
+      <motion.span className="absolute left-0 w-[32.17669%] h-full hidden sm:block z-10 will-change-transform">
+        <Image
+          onLoadingComplete={() =>
+            setPortalLoading((prevState) => prevState + 1)
+          }
+          src={"/img/art/left-portal.png"}
+          layout="fill"
+          priority={true}
+          quality={100}
+        />
+      </motion.span>
+      <motion.span className="absolute right-0 w-[32.23981%] h-full hidden sm:block z-10 will-change-transform">
+        <Image
+          onLoadingComplete={() =>
+            setPortalLoading((prevState) => prevState + 1)
+          }
+          src={"/img/art/right-portal.png"}
+          layout="fill"
+          priority={true}
+          quality={100}
+        />
+      </motion.span>
+      <motion.span className="absolute left-[32.12669%] w-[35.70889%] h-full z-[11] will-change-transform">
+        <Image
+          onLoadingComplete={() =>
+            setPortalLoading((prevState) => prevState + 1)
+          }
+          src={"/img/art/middle-portal.png"}
+          layout="fill"
+          priority={true}
+          quality={100}
+        />
+      </motion.span>
+    </>
+  );
+};
+
+export default Portal;
