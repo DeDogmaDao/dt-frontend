e67cc624248c45c1d358836f24edf75515bbc453
aly3n <aly3n.dev@gmail.com>
2022-04-23 07:52:26 +0430

    refactor(calltoaction): LogoType injected

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -15,7 +15,7 @@ import { dimensionType } from "../../../types/allTypes";
 
 const Hero: React.FC = () => {
   const heroContainerRef = useRef<HTMLDivElement>(null);
-  const [PortalLoading, setPortalLoading] = useState(0);
+  const [portalLoading, setPortalLoading] = useState(0);
   const [dimension, setDimension] = useState<dimensionType>({
     width: 0,
     height: 0,
@@ -82,126 +82,105 @@ const Hero: React.FC = () => {
         w-[100vw] aspect-[947/1492] sm:aspect-auto sm:h-[90vh] md:h-[100vh] lg:h-[56.26vw] mx-auto overflow-hidden hero-container"
       >
         <div className="portal-and-flame-and-hero w-full h-full relative z-20 origin-bottom">
-
-            <motion.div
-                        initial="hidden"
-                        animate={anim}
-                        variants={heroAnim}
-              className=" ml-[-100%] sm:ml-[-25%] lg:ml-auto
+          <motion.div
+            initial="hidden"
+            animate={anim}
+            variants={heroAnim}
+            className=" ml-[-100%] sm:ml-[-25%] lg:ml-auto
              h-full
              w-[300%] sm:w-[150%] lg:w-full
              relative flex justify-center items-center will-change-transform"
+          >
+            <motion.div
+              onHoverStart={() => setAnim("visible")}
+              onHoverEnd={() => setAnim("hidden")}
+              className="red-transparent-on-city absolute w-[20%] h-[65%] top-1/2 mt-[-19.5%] left-1/2 ml-[-10%]  z-100 rounded-[50%] will-change-transform"
+            />
+
+            <motion.span className="absolute left-0 w-[32.17669%] h-full hidden sm:block z-10 will-change-transform">
+              <Image
+                onLoadingComplete={() =>
+                  setPortalLoading((prevState) => prevState + 1)
+                }
+                src={"/img/art/left-portal.png"}
+                layout="fill"
+                priority={true}
+                quality={100}
+              />
+            </motion.span>
+            <motion.span className="absolute right-0 w-[32.23981%] h-full hidden sm:block z-10 will-change-transform">
+              <Image
+                onLoadingComplete={() =>
+                  setPortalLoading((prevState) => prevState + 1)
+                }
+                src={"/img/art/right-portal.png"}
+                layout="fill"
+                priority={true}
+                quality={100}
+              />
+            </motion.span>
+            <motion.span className="absolute left-[32.12669%] w-[35.70889%] h-full z-[11] will-change-transform">
+              <Image
+                onLoadingComplete={() =>
+                  setPortalLoading((prevState) => prevState + 1)
+                }
+                src={"/img/art/middle-portal.png"}
+                layout="fill"
+                priority={true}
+                quality={100}
+              />
+            </motion.span>
+
+            <motion.img
+              className="absolute z-20 left-[48.5%] top-[71%] w-[10%] h-[15%] will-change-transform"
+              src={"/img/art/cape.gif"}
+            />
+            <motion.span
+              style={{
+                x: springX,
+                y: springY,
+                visibility: portalLoading > 2 ? "visible" : "hidden",
+              }}
+              className="absolute w-[48.567%] h-[85.121%] z-0 will-change-transform"
+              initial="hidden"
+              animate={anim}
+              variants={cityAnim}
             >
-              <motion.div
-                onHoverStart={() => setAnim("visible")}
-                onHoverEnd={() => setAnim("hidden")}
-                className="red-transparent-on-city absolute w-[20%] h-[65%] top-1/2 mt-[-19.5%] left-1/2 ml-[-10%]  z-100 rounded-[50%] will-change-transform"
-              />
-              <motion.div
-                onTap={() => setAnim("visible")}
-                onHoverStart={() => setAnim("visible")}
-                onHoverEnd={() => setAnim("hidden")}
-                style={{ visibility: PortalLoading > 2 ? "visible" : "hidden" }}
-                className="mt-[7%] lg:mt-[12%]  absolute flex flex-col justify-center items-center z-110 pb-[2.5%]"
-              >
-                <motion.span className="w-[231px] h-[70px]   will-change-transform">
-                  <Image
-                    src="/img/logo/treasure.png"
-                    layout={"fill"}
-                    quality={100}
-                  />
-                </motion.span>
-                <motion.button
-                  className="text-small-bold  rounded-full w-[138px] h-[40px] flex justify-center items-center text-white
-              bg-neutral-900 hover:bg-neutral-900/60  cursor-pointer will-change-transform duration-500 border-2 border-transparent hover:border-primary-500"
-                >
-                  Join Our Discord
-                </motion.button>
-              </motion.div>
-              <motion.span className="absolute left-0 w-[32.17669%] h-full hidden sm:block z-10 will-change-transform">
-                <Image
-                  onLoadingComplete={() =>
-                    setPortalLoading((prevState) => prevState + 1)
-                  }
-                  src={"/img/art/left-portal.png"}
-                  layout="fill"
-                  priority={true}
-                  quality={100}
-                />
-              </motion.span>
-              <motion.span className="absolute right-0 w-[32.23981%] h-full hidden sm:block z-10 will-change-transform">
-                <Image
-                  onLoadingComplete={() =>
-                    setPortalLoading((prevState) => prevState + 1)
-                  }
-                  src={"/img/art/right-portal.png"}
-                  layout="fill"
-                  priority={true}
-                  quality={100}
-                />
-              </motion.span>
-              <motion.span className="absolute left-[32.12669%] w-[35.70889%] h-full z-[11] will-change-transform">
-                <Image
-                  onLoadingComplete={() =>
-                    setPortalLoading((prevState) => prevState + 1)
-                  }
-                  src={"/img/art/middle-portal.png"}
-                  layout="fill"
-                  priority={true}
-                  quality={100}
-                />
-              </motion.span>
+              <Image src={"/img/art/city.png"} layout="fill" quality={100} />
+            </motion.span>
+            <motion.span
+              style={{
+                opacity: maskOpacity,
+                visibility: portalLoading > 2 ? "visible" : "hidden",
+              }}
+              className="absolute w-full h-full z-0 scale-105 will-change-transform"
+            >
+              <Image src={"/img/art/mask.png"} layout="fill" />
+            </motion.span>
 
-              <motion.img
-                className="absolute z-20 left-[48.5%] top-[71%] w-[10%] h-[15%] will-change-transform"
-                src={"/img/art/cape.gif"}
-              />
-              <motion.span
-                style={{
-                  x: springX,
-                  y: springY,
-                  visibility: PortalLoading > 2 ? "visible" : "hidden",
-                }}
-                className="absolute w-[48.567%] h-[85.121%] z-0 will-change-transform"
-                initial="hidden"
-                animate={anim}
-                variants={cityAnim}
-              >
-                <Image src={"/img/art/city.png"} layout="fill" quality={100} />
-              </motion.span>
-              <motion.span
-                style={{
-                  opacity: maskOpacity,
-                  visibility: PortalLoading > 2 ? "visible" : "hidden",
-                }}
-                className="absolute w-full h-full z-0 scale-105 will-change-transform"
-              >
-                <Image src={"/img/art/mask.png"} layout="fill" />
-              </motion.span>
-
-              <motion.video
-                style={{ visibility: PortalLoading > 2 ? "visible" : "hidden" }}
-                ref={leftFireRef}
-                loop
-                autoPlay
-                muted
-                playsInline
-                className="absolute hidden sm:block border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[14.576%] top-[57.828%] will-change-transform"
-              >
-                <source src={"/img/art/left-fire.mp4"} type="video/mp4" />
-              </motion.video>
-              <motion.video
-                style={{ visibility: PortalLoading > 2 ? "visible" : "hidden" }}
-                ref={rightFireRef}
-                loop
-                autoPlay
-                muted
-                playsInline
-                className="absolute hidden sm:block border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[74.844%] top-[57.828%] will-change-transform"
-              >
-                <source src={"/img/art/right-fire.mp4"} type="video/mp4" />
-              </motion.video>
-            </motion.div>
+            <motion.video
+              style={{ visibility: portalLoading > 2 ? "visible" : "hidden" }}
+              ref={leftFireRef}
+              loop
+              autoPlay
+              muted
+              playsInline
+              className="absolute hidden sm:block border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[14.576%] top-[57.828%] will-change-transform"
+            >
+              <source src={"/img/art/left-fire.mp4"} type="video/mp4" />
+            </motion.video>
+            <motion.video
+              style={{ visibility: PortalLoading > 2 ? "visible" : "hidden" }}
+              ref={rightFireRef}
+              loop
+              autoPlay
+              muted
+              playsInline
+              className="absolute hidden sm:block border-none outline-none z-20 w-[9.429%] h-[20.114%] left-[74.844%] top-[57.828%] will-change-transform"
+            >
+              <source src={"/img/art/right-fire.mp4"} type="video/mp4" />
+            </motion.video>
+          </motion.div>
         </div>
       </motion.div>
     </>
