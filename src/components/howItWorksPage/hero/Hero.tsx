fe31e1a346df0e37d3d07a51df1de64aa9e35224
aly3n <aly3n.dev@gmail.com>
2022-04-24 02:04:26 +0430

    some fix

--- src/components/howItWorksPage/hero/Hero.tsx
+++ src/components/howItWorksPage/hero/Hero.tsx
@@ -66,7 +66,7 @@ const Hero: React.FC = () => {
     const fisaghores =
       Math.pow(event.pageX - dimension.width / 2, 2) +
       Math.pow(event.pageY - dimension.height / 2, 2);
-    maskOpacity.set(Math.sqrt(fisaghores) / (dimension.width / 1.1));
+    maskOpacity.set(Math.sqrt(fisaghores) / (dimension.width / 1.1) -0.1);
   };
   const touchMoveHandler = (event: TouchEvent) => {
     xCord.set(event.touches[0].pageX);
