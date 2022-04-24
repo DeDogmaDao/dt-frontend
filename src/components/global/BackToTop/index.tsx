ea445d7f2abcd33a102242d55b46a79542f08325
aly3n <aly3n.dev@gmail.com>
2022-04-24 08:38:57 +0430

    feat(backToTop): animation added

--- src/components/global/BackToTop/index.tsx
+++ src/components/global/BackToTop/index.tsx
@@ -1,5 +1,6 @@
-import { motion, useViewportScroll } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { RefObject, useEffect, useState } from "react";
+import { backToTopAni } from "../../../utils/animation";
 import BackToTopSVG from "../../svgs/backtotop.svg";
 interface props {
   containerRef: RefObject<HTMLDivElement>;
@@ -25,24 +26,30 @@ const BackToTop: React.FC<props> = ({ containerRef }) => {
       window.removeEventListener("resize", resizeHandler);
     };
   }, []);
-  
+
   return (
     <>
-      {scrollY / scrollHeight > 0.8 && (
-        <motion.button
-          className="group z-[200] flex flex-col justify-center items-center w-[4.6875rem] aspect-[75/84]
+      <AnimatePresence>
+        {(scrollY / scrollHeight) > 0.8 && (
+          <motion.button
+            initial="hidden"
+            animate="visible"
+            exit="out"
+            variants={backToTopAni}
+            className="group z-[200] flex flex-col justify-center items-center w-[4.6875rem] aspect-[75/84]
        fixed right-[4.875rem] bottom-[6.375rem] hover:text-primary-500 duration-200 hover:scale-105 active:scale-95
         will-change-transform"
-        >
-          <span
-            className=" will-change-transform stroke-primary-500 w-[3.125rem] aspect-[5/6] fill-transparent
-        group-hover:stroke-secondary-200 duration-200"
           >
-            <BackToTopSVG />
-          </span>
-          <div className="text-sm font-normal">Back to Top</div>
-        </motion.button>
-      )}
+            <span
+              className=" will-change-transform stroke-primary-500 w-[3.125rem] aspect-[5/6] fill-transparent
+        group-hover:stroke-secondary-200 duration-200"
+            >
+              <BackToTopSVG />
+            </span>
+            <div className="text-sm font-normal">Back to Top</div>
+          </motion.button>
+        )}
+      </AnimatePresence>
     </>
   );
 };
