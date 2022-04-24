2e709b9f65f6e3598400f9426acb1b0fde180edd
aly3n <aly3n.dev@gmail.com>
2022-04-25 00:25:55 +0430

    refactor(images): static import for road image finished

--- src/components/roadMapPage/Road.tsx
+++ src/components/roadMapPage/Road.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { roadMapType } from "../../types/allTypes";
 import { roadAni, roadMapDescAni } from "../../utils/animation";
 import RoadItem from "./RoadItem";
+import roadImages from './roadImg';
 
 interface props {
   road: roadMapType;
@@ -36,7 +37,7 @@ const Road: React.FC<props> = ({ index, activeSection, road }) => {
       >
       <span className="absolute w-full aspect-[1426/821]">
         <span className="w-full h-full inner-image-no-max-width">
-          <Image src={road.backImg} layout="fill" quality={100}  />
+          <Image src={roadImages[index]} layout="fill" quality={100} placeholder="blur"  />
         </span>
       </span>
         <h3 className="w-full h-1/3 lg:w-1/2 lg:h-full pt-20 lg:pt-0 z-10 flex justify-center items-center 
