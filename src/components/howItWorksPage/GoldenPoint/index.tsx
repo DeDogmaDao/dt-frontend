f90c0826c19eaf75d190ca921c48eedd862f97cd
aly3n <aly3n.dev@gmail.com>
2022-04-25 23:35:02 +0430

    goldenpoint added

---
+++ src/components/howItWorksPage/GoldenPoint/index.tsx
@@ -0,0 +1,13 @@
+const GoldenPoint: React.FC = () => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div>Demmortal Treasure is more than a Collectable</div>
+      <div>
+        Sustainable economy empowered by Anti-Rug Protocols and a Daily Lottery
+        with guaranteed prize. All by owning a NFT!
+      </div>
+    </div>
+  );
+};
+
+export default GoldenPoint;
