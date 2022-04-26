435f99bc62f64f79604e5d762a4bb9e218c2a5a3
aly3n <aly3n.dev@gmail.com>
2022-04-26 12:57:24 +0430

    footer fix1

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import Image from "next/image";
 import { footerData } from "../../../store/allData";
 import Social from "./Social";
+import treasure from '../../images/logo/treasure.png';
 const Footer: React.FC = () => {
   return (
     <>
@@ -10,7 +11,7 @@ const Footer: React.FC = () => {
         className="h-full w-full flex flex-col justify-center items-center z-100  relative -mt-40 sm:-mt-56"
       >
         <div className="scale-[80%] ssm:scale-100  z-10 mb-[-3rem] w-[25.25rem] h-[15.23rem]">
-          <Image src="/img/logo/treasure.png" layout="fill"  />
+          <Image src={treasure} layout="fill" placeholder="blur"  />
         </div>
         <h3 className="mb-3 text-[1.25rem] ssm:text-[1.75rem] sm:text-[2.375rem] font-bold sm:font-bold z-10">
           NFT Game and Collection by
