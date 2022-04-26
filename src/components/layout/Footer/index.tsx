39c8ed0b92a4e4b9642cec9d4ccc77adf6a8e5f4
aly3n <aly3n.dev@gmail.com>
2022-04-26 12:51:32 +0430

    footer fixed

--- src/components/layout/Footer/index.tsx
+++ src/components/layout/Footer/index.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
         layout
         className="h-full w-full flex flex-col justify-center items-center z-100  relative -mt-40 sm:-mt-56"
       >
-        <div className="scale-[80%] ssm:scale-100  z-10 mb-[-3rem] w-[25.25rem] aspect-[1207/728]">
+        <div className="scale-[80%] ssm:scale-100  z-10 mb-[-3rem] w-[25.25rem] h-[15.23rem]">
           <Image src="/img/logo/treasure.png" layout="fill"  />
         </div>
         <h3 className="mb-3 text-[1.25rem] ssm:text-[1.75rem] sm:text-[2.375rem] font-bold sm:font-bold z-10">
