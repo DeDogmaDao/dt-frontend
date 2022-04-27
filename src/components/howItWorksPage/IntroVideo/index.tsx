2c82cb7e52b52c32f228c5603536163a7cdea51d
aly3n <aly3n.dev@gmail.com>
2022-04-27 17:25:10 +0430

    video with double click on touch bug

--- src/components/howItWorksPage/IntroVideo/index.tsx
+++ src/components/howItWorksPage/IntroVideo/index.tsx
@@ -5,40 +5,17 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import { videoBtnAnim } from "../../../utils/animation";
 import introBg from "../../images/bg/sec2.png";
+import videoBg from "../../images/bg/Story.png";
 
 const InteroVideo: React.FC = () => {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [iseHovering, setIsHovering] = useState(false);
-  const [isTapped, setIsTapped] = useState(0);
-  const [showBtn, setShowBtn] = useState(true);
   const [once, setOnce] = useState(false);
   const videoRef = useRef<HTMLIFrameElement>(null);
   const videoClickHandler = () => {
     if (once === false) {
       setOnce(true);
+      videoRef.current!.src = videoRef.current!.src + "?autoplay=1?muted=1";
     }
-    setIsPlaying((prevState) => !prevState);
   };
-
-  // useEffect(() => {
-  //   if (isPlaying === false) {
-  //     videoRef.current!.pause();
-  //   } else {
-  //     videoRef.current!.play();
-  //   }
-  //   setShowBtn(true);
-  // }, [isPlaying]);
-
-  // useEffect(() => {
-  //   setIsHovering(true);
-  //   const btnTimeOut = setTimeout(() => {
-  //     setIsHovering(false);
-  //   }, 2000);
-
-  //   return () => {
-  //     clearTimeout(btnTimeOut);
-  //   };
-  // }, [isTapped]);
 
   return (
     <div className="w-full aspect-[16/13] sm:aspect-video flex justify-center items-center relative">
@@ -50,40 +27,66 @@ const InteroVideo: React.FC = () => {
         </span>
       </span>
       <motion.div
-        className="aspect-video w-[90%] sm:w-[70%] rounded-[30px] z-10 mt-[-0%] flex justify-center items-center
-      relative cursor-pointer bg-red-50/40 overflow-hidden"
-      onClick={()=>{videoRef.current!.src="https://www.youtube.com/embed/sP13ARp0jTA?autoplay=1"}}
+        onClick={videoClickHandler}
+        className="aspect-video w-[90%] sm:w-[70%] rounded-[2.2vw] z-10 mt-[-0%] flex justify-center items-center
+      relative cursor-pointer bg-neutral-900 overflow-hidden"
       >
-      <iframe
-      className="w-full h-full"
-      ref={videoRef}
-      loading="lazy"
-      src={``}
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-      allowFullScreen
-      title="Demmortal Treasure dedogmadao"
-    />
+        <iframe
+        
+          allowTransparency={true}
+          className="w-full h-full z-10"
+          ref={videoRef}
+          loading="lazy"
+          src={`https://www.youtube.com/embed/sP13ARp0jTA`}
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Demmortal Treasure dedogmadao"
+        />
         {!once && (
-          <motion.div
-            onTap={() => setIsTapped((prevState) => prevState + 1)}
-            className="absolute w-[100%] h-[10rem] mt-[30%]  flex flex-col justify-center items-center z-20
+          <div className="absolute top-0 left-0 w-full h-full z-20">
+            <div className="relative w-full h-full flex justify-center items-center">
+              <motion.span
+                initial="hidden"
+                animate="visible"
+                exit="out"
+                variants={videoBtnAnim}
+                className="z-20 absolute w-[3rem] md:w-[5.625rem] aspect-square p-2 md:p-6 rounded-full bg-white/20 fill-white
+          hover:scale-125 hover:fill-primary-500 duration-150 ease-out"
+              >
+                <PlaySVG />
+              </motion.span>
+
+              <span className="w-full  absolute h-full lg:aspect-[1382/814]">
+                <span className="w-full h-full inner-image-no-max-width">
+                  <Image
+                    src={videoBg}
+                    layout="fill"
+                    quality={100}
+                    placeholder="blur"
+                  />
+                </span>
+              </span>
+              <motion.div
+                className="absolute w-[100%] h-[10rem] mt-[30%]  flex flex-col justify-center items-center z-20
         gap-y-1 pointer-events-none scale-[38%] md:scale-[60%] lg:scale-100"
-          >
-            <span className="w-[26px] h-[6px] bg-primary-500" />
-            <span className="text-[2.5rem] font-bold whitespace-nowrap">
-              Odyssey of the{" "}
-              <span className="text-primary-500">Titan’s treasure</span>
-            </span>
-            <span className="text-xl font-light text-center md:px-10">
-              Olympians, Titans and mortals- united in search of fortune. Watch
-              to see how it all came to be
-            </span>
-          </motion.div>
+              >
+                <span className="w-[26px] h-[6px] bg-primary-500" />
+                <span className="text-[2.5rem] font-bold whitespace-nowrap">
+                  Odyssey of the{" "}
+                  <span className="text-primary-500">Titan’s treasure</span>
+                </span>
+                <span className="text-xl font-light text-center md:px-10">
+                  Olympians, Titans and mortals- united in search of fortune.
+                  Watch to see how it all came to be
+                </span>
+              </motion.div>
+            </div>
+          </div>
         )}
         <img
           src="/img/video/border.png"
-          className="w-full aspect-video pl-px absolute"
+          className="w-full aspect-video pl-px absolute z-30 pointer-events-none"
         />
       </motion.div>
     </div>
