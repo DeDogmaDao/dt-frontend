58e3c9495b4e61ec1dd09163cbd1dd45198f717a
aly3n <aly3n.dev@gmail.com>
2022-04-28 06:18:58 +0430

    fix: discord buttons

--- src/components/howItWorksPage/hero/LogoType.tsx
+++ src/components/howItWorksPage/hero/LogoType.tsx
@@ -17,12 +17,14 @@ const LogoType: React.FC<props> = ({ setAnim, portalLoading }) => {
       <motion.span className="w-[20rem] aspect-[1207/728]   will-change-transform">
         <Image src={treasure} layout={"fill"} quality={75} placeholder="blur" />
       </motion.span>
-      <motion.button
+      <motion.a
+      href="https://discord.gg/dedogmadao"
+      target={"_blank"}
         className="text-small-bold  rounded-full w-[8.625rem] h-[2.5rem] flex justify-center items-center text-white mt-[-14%]
         bg-neutral-900 hover:bg-neutral-900/60  cursor-pointer will-change-transform duration-500 border-2 border-transparent hover:border-primary-500"
       >
         Join Our Discord
-      </motion.button>
+      </motion.a>
     </motion.div>
   );
 };
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -65,12 +65,14 @@ const Header: React.FC = () => {
           </span>
           <span>Litepaper</span>
         </a>
-        <button
-          className="z-50 absolute pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600
+        <a
+                  href="https://discord.gg/dedogmadao"
+                  target="_blank"
+          className="z-50 absolute cursor-pointer pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600
          text-white text-xl flex justify-center items-center hover:bg-neutral-500 duration-200 fill-primary-500 hover:fill-primary-700"
         >
           <DiscordSVG width={26} />
-        </button>
+        </a>
         <MobileMenu headerLinks={headerLinks} />
       </div>
     </div>
