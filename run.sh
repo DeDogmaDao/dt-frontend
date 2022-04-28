9002dd4adbf358d040f4c3f344407c30a3e2b779
demonic-dev <demonic-dev@protonmail.com>
2022-04-29 03:56:02 +0430

    removed e on echo

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 touch /app/frontend/src/public/robots.txt
-echo -e "$ROBOTS_TXT" > /app/frontend/src/public/robots.txt
+echo "$ROBOTS_TXT" > /app/frontend/src/public/robots.txt
 npm run up
