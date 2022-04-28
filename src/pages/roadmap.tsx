237bb1c9cfbe533eab7a4ff6654d7f9f8f188e86
aly3n <aly3n.dev@gmail.com>
2022-04-28 14:19:33 +0430

    feat(metadata):metadata added to roadmap

--- src/pages/roadmap.tsx
+++ src/pages/roadmap.tsx
@@ -1,19 +1,33 @@
 import Head from "next/head";
 import { Fragment } from "react";
 import RoadMapPage from "../components/roadMapPage/RoadMapPage";
+import { roadmapPageMetaData } from "../store/allData";
 
 export default function roadmap(): JSX.Element {
   return (
     <Fragment>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {/* {careerMeta.preload.map((src) => (
-          <link key={src} rel="preload" as="image" href={src} />
-        ))} */}
-        {/* <title></title> */}
-        {/* <meta name="description" content={} /> */}
-        {/* <meta name="keywords" content={careerMeta.keywords} />
-        <meta name="author" content={careerMeta.author} /> */}
+      <title>{"DDD" + " | " + roadmapPageMetaData.pageTitle}</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content={roadmapPageMetaData.desc} />
+        <meta name="keywords" content={roadmapPageMetaData.keywords} />
+        <link rel="canonical" href={roadmapPageMetaData.pageUrl} />
+        <meta property="og:url" content={roadmapPageMetaData.pageUrl} />
+        <meta property="og:image" content={roadmapPageMetaData.imageUrl} />
+        <meta property="og:description" content={roadmapPageMetaData.desc} />
+        <meta
+          property="og:title"
+          content={
+            roadmapPageMetaData.siteTitle + " | " + roadmapPageMetaData.pageTitle
+          }
+        />
+        <meta property="og:site_name" content={roadmapPageMetaData.siteTitle} />
+        <meta property="og:see_also" content={roadmapPageMetaData.homePageUrl} />
+        <meta name="twitter:card" content={roadmapPageMetaData.desc} />
+        <meta name="twitter:url" content={roadmapPageMetaData.pageUrl} />
+        <meta name="twitter:title" content={roadmapPageMetaData.pageTitle} />
+        <meta name="twitter:description" content={roadmapPageMetaData.desc} />
+        <meta name="twitter:image" content={roadmapPageMetaData.imageUrl} />
       </Head>
       <RoadMapPage />
     </Fragment>
