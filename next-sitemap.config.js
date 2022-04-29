a9c837e98c04709bdb51fdc3f713a0eb026c94ed
aly3n <aly3n.dev@gmail.com>
2022-04-29 07:14:32 +0430

    feat:auto sitemap generator

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://dedogmadao.com",
+  changefreq: 'weekly',
+  priority: 1,
+  exclude: ['/gameboard', '/gallery',"/underconstruction"],
+};
