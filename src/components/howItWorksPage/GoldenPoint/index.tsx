2050447e707f6f9aeabd83c1d0e95f665f60ac59
aly3n <aly3n.dev@gmail.com>
2022-04-29 07:20:50 +0430

    fix:add h2 instead of h3 for sections

--- src/components/howItWorksPage/GoldenPoint/index.tsx
+++ src/components/howItWorksPage/GoldenPoint/index.tsx
@@ -1,28 +1,15 @@
 import { motion } from "framer-motion";
 import { useOnScrollAnimation } from "../../../hooks/useOnScrollAnimation";
-import {
-  goldenSecondAni,
-} from "../../../utils/animation";
+import { goldenSecondAni } from "../../../utils/animation";
 
 const GoldenPoint: React.FC = () => {
   const [element, controls] = useOnScrollAnimation(0.5);
   return (
     <motion.div className="flex flex-col justify-center items-center z-100 px-5 pt-10">
-      <motion.div className="flex flex-wrap justify-center gap-x-2 text-[1.75rem] font-bold">
-        {"Demmortal Treasure is more than just a"
-          .split(" ")
-          .map((word, index) => {
-            return <motion.span key={word + index}>{word}</motion.span>;
-          })}
-
-        <motion.div className="flex justify-between whitespace-nowrap">
-          {"Collectible".split("").map((word, index) => {
-            return (
-              <motion.span key={word + index} className="text-primary-500">{word}</motion.span>
-            );
-          })}
-        </motion.div>
-      </motion.div>
+      <motion.h1 className="flex flex-wrap justify-center gap-x-2 text-[1.75rem] font-bold">
+        Demmortal Treasure is more than just a
+        <motion.span className="text-primary-500">Collectible</motion.span>
+      </motion.h1>
       <motion.div
         initial="hidden"
         variants={goldenSecondAni}
@@ -35,7 +22,7 @@ const GoldenPoint: React.FC = () => {
           .map((word, index) => {
             return (
               <span
-              key={word + index}
+                key={word + index}
                 className={`whitespace-nowrap ${
                   (index === 6 ||
                     index === 7 ||
