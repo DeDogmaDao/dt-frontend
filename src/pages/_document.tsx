ff428c70b405dd83e57e9442c56351381198b9b4
aly3n <aly3n.dev@gmail.com>
2022-04-29 07:44:44 +0430

    feat:add google tag manager script and noscript into _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+const scriptTagManager: string = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KQ3KRW6');`;
+const noScriptTagManager: string = `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KQ3KRW6" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 export default function Document() {
   return (
     <Html>
       <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: scriptTagManager,
+          }}
+        />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
@@ -13,6 +19,11 @@ export default function Document() {
       </Head>
 
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: noScriptTagManager,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
