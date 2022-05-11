8c7239a9cf22562af5dd8f0ec16ad2f1239c0f11
aly3n <aly3n.dev@gmail.com>
2022-05-11 14:40:50 +0430

    change images for team and benefits

--- src/store/img.ts
+++ src/store/img.ts
@@ -1,13 +1,13 @@
 import { StaticImageData } from "next/image";
 
 // teams
-import member1 from "../components/images/team/member1.png";
-import member2 from "../components/images/team/member2.png";
-import member3 from "../components/images/team/member3.png";
-import member4 from "../components/images/team/member4.png";
-import member5 from "../components/images/team/member5.png";
-import member6 from "../components/images/team/member6.png";
-import member7 from "../components/images/team/member7.png";
+import member1 from "./images/team/member1.png";
+import member2 from "./images/team/member2.png";
+import member3 from "./images/team/member3.png";
+import member4 from "./images/team/member4.png";
+import member5 from "./images/team/member5.png";
+import member6 from "./images/team/member6.png";
+import member7 from "./images/team/member7.png";
 export const teamImg: StaticImageData[] = [
   member1,
   member2,
@@ -19,8 +19,8 @@ export const teamImg: StaticImageData[] = [
 ];
 
 // benefits
-import floor from "../components/images/benefits/floor.png";
-import lottery from "../components/images/benefits/lottery.png";
-import pool from "../components/images/benefits/pool.png";
+import floor from "./images/benefits/floor.png";
+import lottery from "./images/benefits/lottery.png";
+import pool from "./images/benefits/pool.png";
 export const benefitImg: StaticImageData[] = [floor, lottery, pool];
 
