6c1a5063516908c6eaaafaf83b7728484c2e6ddb
aly3n <aly3n.dev@gmail.com>
2022-05-11 14:57:24 +0430

    refactor(images): transfer svg to stor

--- src/store/allData.tsx
+++ src/store/allData.tsx
@@ -15,18 +15,17 @@ import {
   metaDataType,
 } from "../types/allTypes";
 
+// images
+import { socialSVGs } from "./svg";
 //
 //
 //
-//
-import DiscordSVG from "../components/svgs/discord.svg";
-import YoutubeSVG from "../components/svgs/youtube.svg";
-import Twitter from "../components/svgs/twitter.svg";
-import Github from "../components/svgs/github.svg";
-import Instagram from "../components/svgs/instagram.svg";
-import Linkedin from "../components/svgs/linkedin.svg";
 
 import { teamImg } from "./img";
+
+const { DiscordSVG, YoutubeSVG, Twitter, Github, Instagram, Linkedin } =
+  socialSVGs;
+
 export const footerData: socialType[] = [
   {
     icon: <YoutubeSVG />,
