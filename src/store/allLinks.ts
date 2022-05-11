93aa6eff360b1b418f92b50b9b92dc6b063ac920
aly3n <aly3n.dev@gmail.com>
2022-05-11 16:13:43 +0430

    refactor(images):add team link

--- src/store/allLinks.ts
+++ src/store/allLinks.ts
@@ -1,15 +1,48 @@
 ////////social links
-export const discordLink:string = "https://discord.gg/8dVfHQfxpq";
-export const youtubeLink:string = "https://www.youtube.com/channel/UCNGZXAcCP-xHrAVqaEWqozw";
-export const twitterLink:string = "https://twitter.com/DMTtreasure";
-export const gitHubLink:string = "https://github.com/DeDogmaDao";
-
+export const discordLink: string = "https://discord.gg/8dVfHQfxpq";
+export const youtubeLink: string =
+  "https://www.youtube.com/channel/UCNGZXAcCP-xHrAVqaEWqozw";
+export const twitterLink: string = "https://twitter.com/DMTtreasure";
+export const gitHubLink: string = "https://github.com/DeDogmaDao";
 
 //////// faq links
 //Do the NFTs have an equal chance to win the lottery?
-export const lotteryChanceLink:string = "https://docs.dedogmadao.com/game-structure#guilds-formed-by-special-powers-gifted-by-the-olympian-gods";
+export const lotteryChanceLink: string =
+  "https://docs.dedogmadao.com/game-structure#guilds-formed-by-special-powers-gifted-by-the-olympian-gods";
 
 // Will Demmortal Treasure be minted on ERC-721A standard?
-export const erc721Link:string = "https://www.azuki.com/erc721a";
+export const erc721Link: string = "https://www.azuki.com/erc721a";
 
-//
+//////// team members contacts
+//Decentral Titan
+export const decentralTitanSocial = {
+  twitter: "https://twitter.com/DecentralTitan",
+};
+
+//metiEth
+export const metiEthSocial = {
+    linkedin:"https://www.linkedin.com/in/mehdi-salehi-b16873158/"
+}
+
+//sara
+export const saraSocial = {
+    linkedin:"https://www.linkedin.com/in/sara-rabiee-17711260/"
+}
+
+//george
+export const georgeSocial = {
+    linkedin:"https://www.linkedin.com/in/george-boyle-845b38b8/",
+    github:"https://github.com/Dockheas23"
+}
+
+//flokiBB
+export const flokiBBSocial = {
+    twitter:"https://twitter.com/FlokiTheBB",
+    github:"https://github.com/FlokiBB/"
+}
+
+//katy
+export const katySocial = {
+    instagram: "https://www.instagram.com/katythemaker"
+}
+
