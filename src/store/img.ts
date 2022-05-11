2baf3e2d212751d8d8918514560a9716077350db
aly3n <aly3n.dev@gmail.com>
2022-05-11 17:05:49 +0430

    refactor(images):change images of tabs

--- src/store/img.ts
+++ src/store/img.ts
@@ -54,3 +54,42 @@ import city from "./images/art/city.png";
 import mask from "./images/art/mask.png";
 
 export const allArtImg = { portal, middlePortal, city, mask };
+
+
+//tabs
+import bandits from "./images/tabs/bandits.png";
+import beastriders from "./images/tabs/beast-riders.png";
+import enchants from "./images/tabs/enchants.png";
+import human1 from "./images/tabs/human1.png";
+import human2 from "./images/tabs/human2.png";
+import smiths from "./images/tabs/smiths.png";
+import aphrodite from "./images/tabs/aphrodite.jpg";
+import ares from "./images/tabs/ares.jpg";
+import artemis from "./images/tabs/artemis.jpg";
+import hephaestus from "./images/tabs/hephaestus.jpg";
+import hades from "./images/tabs/hades.jpg";
+import hecate from "./images/tabs/hecate.jpg";
+import hera from "./images/tabs/hera.jpg";
+import hermes from "./images/tabs/hermes.jpg";
+import poseidon from "./images/tabs/poseidon.jpg";
+import zeus from "./images/tabs/zeus.jpg";
+
+interface tabImgDataType {
+  [gods: string]: StaticImageData[];
+  humans: StaticImageData[];
+}
+export const tabImgData:tabImgDataType = {
+  humans: [beastriders, bandits, enchants, smiths, human1, human2],
+  gods: [
+    zeus,
+    artemis,
+    hermes,
+    hephaestus,
+    hecate,
+    hades,
+    aphrodite,
+    poseidon,
+    ares,
+    hera,
+  ],
+};
