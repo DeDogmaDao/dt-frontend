947793b8df0b5dc8118c1a53d05853eeb9ccf12f
aly3n <aly3n.dev@gmail.com>
2022-05-15 18:53:02 +0430

    feat(web3): add web3state reducer error handling

--- src/store/reducers/Web3Provider.ts
+++ src/store/reducers/Web3Provider.ts
@@ -1,8 +1,38 @@
-import { Web3ProviderState } from "../../types/allTypes";
+import { Web3Action, Web3ProviderState } from "../../types/allTypes";
 
 export const web3InitialState: Web3ProviderState = {
-    provider: null,
-    web3Provider: null,
-    address: null,
-    network: null,
+  provider: null,
+  web3Provider: null,
+  address: null,
+  network: null,
+};
+
+export function web3Reducer(
+  state: Web3ProviderState,
+  action: Web3Action
+): Web3ProviderState {
+  switch (action.type) {
+    case "SET_WEB3_PROVIDER":
+      return {
+        ...state,
+        provider: action.provider,
+        web3Provider: action.web3Provider,
+        address: action.address,
+        network: action.network,
+      };
+    case "SET_ADDRESS":
+      return {
+        ...state,
+        address: action.address,
+      };
+    case "SET_NETWORK":
+      return {
+        ...state,
+        network: action.network,
+      };
+    case "RESET_WEB3_PROVIDER":
+      return web3InitialState;
+    default:
+      throw new Error(`Unhandled action type web3Provider reducer: ${action}`);
   }
+}
--- src/types/allTypes.ts
+++ src/types/allTypes.ts
@@ -155,3 +155,23 @@ export interface Web3ProviderState {
   address: string | null | undefined;
   network: ethers.providers.Network | null | undefined;
 }
+
+export type Web3Action =
+  | {
+      type: "SET_WEB3_PROVIDER";
+      provider?: Web3ProviderState["provider"];
+      web3Provider?: Web3ProviderState["web3Provider"];
+      address?: Web3ProviderState["address"];
+      network?: Web3ProviderState["network"];
+    }
+  | {
+      type: "SET_ADDRESS";
+      address?: Web3ProviderState["address"];
+    }
+  | {
+      type: "SET_NETWORK";
+      network?: Web3ProviderState["network"];
+    }
+  | {
+      type: "RESET_WEB3_PROVIDER";
+    };
