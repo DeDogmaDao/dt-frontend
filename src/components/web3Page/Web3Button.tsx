c75cbc3a0312a03c06b96d0758971b58c4e4d2ea
aly3n <aly3n.dev@gmail.com>
2022-05-28 23:51:18 +0430

    feat(web3):add mint nft option

--- src/components/web3Page/Web3Button.tsx
+++ src/components/web3Page/Web3Button.tsx
@@ -1,11 +1,13 @@
 interface props {
     onclick:any;
+    name:string;
 }
-const Web3Button:React.FC<props> =({onclick}) => {
+const Web3Button:React.FC<props> =({onclick,name}) => {
 
   return (
-      <button onClick={onclick}>
-          connect to network
+      <button className="px-5 py-1 rounded-full bg-green-300 text-black hover:text-white hover:bg-green-700 duration-300 active:scale-95"
+       onClick={onclick}>
+          {name}
       </button>
   ) 
   
--- src/components/web3Page/index.tsx
+++ src/components/web3Page/index.tsx
@@ -3,43 +3,58 @@ import { useEffect } from "react";
 import Web3Button from "./Web3Button";
 import {useWeb3Context} from "../../store/context/Web3Context";
 import { BigNumber, utils } from "ethers";
-import { useContractRead } from 'wagmi';
 import { deDogmaDaoABI } from "./abi";
-import { useSendTransaction } from 'wagmi';
-
+import { useSendTransaction, useSigner } from 'wagmi';
+import { useContractWrite, useWaitForTransaction } from 'wagmi';
 
 const Web3Page:React.FC = () => {
 
     const prov = useWeb3Context();
+    const { data: signer } = useSigner();
+    const {data, write, isSuccess, } = useContractWrite({
+      addressOrName: '0x663F3ad617193148711d28f5334eE4Ed07016602',
+      contractInterface: deDogmaDaoABI,
+      signerOrProvider: signer,
+    },
+    "publicMint",
+    {
+      args:[2],
+      overrides:{
+        value: utils.parseEther("0.14"),
+        gasLimit: 200000,
+      }
+    }
+    )
+      // const { data, isIdle, isError, isLoading, isSuccess, sendTransaction } =
+      // useSendTransaction({
+      //   request: {
+      //     to: '0xe3af8b5Bdd1155e78E67Be558C04D3fa6647365d',
+      //     value: BigNumber.from('100000000000000000000'), // 1 ETH
+      //   },
+      // })
 
-    const contractRead = useContractRead(
-        {
-          addressOrName: '0xe3af8b5Bdd1155e78E67Be558C04D3fa6647365d',
-          contractInterface: deDogmaDaoABI,
-        },
-        'publicMint',
-      );
-      const { data, isIdle, isError, isLoading, isSuccess, sendTransaction } =
-      useSendTransaction({
-        request: {
-          to: '0xe3af8b5Bdd1155e78E67Be558C04D3fa6647365d',
-          value: BigNumber.from('1000000000000000000'), // 1 ETH
-        },
+      const {data:waitedData } = useWaitForTransaction({
+        hash: data?.hash
       })
-      
+      console.log(waitedData)
     return (
         <div className="w-full h-screen flex flex-col justify-center items-center">
-            <div className="w-1/2 h-1/2 bg-red-200/50 flex justify-center items-center">
-            <Web3Button onclick={()=>{
+            <div className="w-1/2 h-1/2  flex flex-col gap-5 justify-center items-center">
+            <Web3Button name="connect" onclick={()=>{
                 prov.metaMaskConnection();
-                sendTransaction();
-                console.log(data);
-            
-            
+            }} />
+            <Web3Button name="disconnect" onclick={()=>{
+                prov.disconnection();
+            }} />
+            <Web3Button name="send" onclick={ ()=>{
+                write();
+                console.log(waitedData)
+                
+
             }} />
 
             </div>
-            <div className="w-1/2 h-1/2 bg-red-200/50 flex flex-col justify-center items-center gap-5">
+            <div className="w-1/2 h-1/2 flex flex-col justify-center items-center gap-5">
                 {/* <div>{address}</div> */}
                 {/* <div>{network}</div>
                 <div>{provider}</div>
