1d001718f49797379bf744f6e259225221aed500
aly3n <aly3n.dev@gmail.com>
2022-05-31 21:41:04 +0430

    feat(hook): add modal to the wallet section

--- src/components/global/ConnectWalletModal/index.tsx
+++ src/components/global/ConnectWalletModal/index.tsx
@@ -1,35 +1,59 @@
-import { useEffect } from "react";
+import { MouseEvent, MouseEventHandler, useEffect, useState } from "react";
 // import { useWeb3Context } from "../../store/context/Web3Context";
 import Web3Button from "./Web3Button";
-import {useWeb3Context} from "../../../store/context/Web3Context";
+import { useWeb3Context } from "../../../store/context/Web3Context";
 import { useWeb3Contract } from "../../../hooks/useWeb3Contract";
+import Modal from "../Modal";
 
-const ConnectWalletModal:React.FC = () => {
+const ConnectWalletModal: React.FC = () => {
+  const [isOpenModal, setIsOpenModal] = useState(true);
 
-    const prov = useWeb3Context();
-    const {write, waitedData} = useWeb3Contract({functionName:"publicMint",args:[2],ethersValue:"1.5"})
-    return (
-        <div className="w-screen h-screen fixed flex flex-col justify-center items-center z-[11000]">
-            <div className="relative w-full h-full flex justify-center items-center backdrop-blur-[.8125rem]">
-                <div className="w-[30.8125rem] h-[30.1875rem] bg-red-500">
-                <div className="w-1/2 h-1/2  flex flex-col gap-5 justify-center items-center">
-            <Web3Button name="connect" onclick={()=>{
+  const prov = useWeb3Context();
+  const { write, waitedData } = useWeb3Contract({
+    functionName: "publicMint",
+    args: [2],
+    ethersValue: "1.5",
+  });
+
+  const modalBackdropClickHandler: MouseEventHandler = (e) => {
+    const el = e.target as HTMLDivElement;
+    e.stopPropagation();
+    if (el.parentElement?.parentElement!.matches("#connect-wallet-modal")) {
+      setIsOpenModal(false);
+    }
+  };
+  return (
+    <Modal
+      isOpen={isOpenModal}
+      onBackdropClick={(e) => modalBackdropClickHandler(e)}
+    >
+      <div className="relative w-full h-full flex justify-center items-center backdrop-blur-[.8125rem]">
+        <div className="w-[30.8125rem] h-[30.1875rem] bg-red-500">
+          <div className="w-1/2 h-1/2  flex flex-col gap-5 justify-center items-center">
+            <Web3Button
+              name="connect"
+              onclick={() => {
                 prov.metaMaskConnection();
-            }} />
-            <Web3Button name="disconnect" onclick={()=>{
+              }}
+            />
+            <Web3Button
+              name="disconnect"
+              onclick={() => {
                 prov.disconnection();
-            }} />
-            <Web3Button name="send" onclick={ ()=>{
+              }}
+            />
+            <Web3Button
+              name="send"
+              onclick={() => {
                 write();
-                console.log(waitedData)
-            }} />
-
-            </div>
-
-                </div>
-            </div>
+                console.log(waitedData);
+              }}
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+    </Modal>
+  );
+};
 
 export default ConnectWalletModal;
--- src/components/global/Modal/index.tsx
+++ src/components/global/Modal/index.tsx
@@ -1,15 +1,32 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { MouseEvent, MouseEventHandler, useEffect } from "react";
 import ClientOnly from "../../../hoc/ClientOnly";
 import Portal from "../Portal";
-
-const Modal:React.FC = () =>{
-
-    return(
-        <ClientOnly>
-            <Portal id="connect-wallet-modal">
-                
-            </Portal>
-        </ClientOnly>
-    );
+interface props {
+  isOpen: boolean;
+  onBackdropClick: MouseEventHandler<HTMLDivElement> | undefined;
 }
+const Modal: React.FC<props> = ({ isOpen, onBackdropClick, children }) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
+
+  return (
+    <ClientOnly>
+      <Portal id="connect-wallet-modal">
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className="overflow-hidden fixed top-0 left-0 w-screen h-screen z-[15000] backdrop-blur-[.8125rem]"
+              onClick={onBackdropClick}
+            >
+              {children}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </Portal>
+    </ClientOnly>
+  );
+};
 
 export default Modal;
