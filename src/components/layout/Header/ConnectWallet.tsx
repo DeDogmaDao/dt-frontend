50b61099efa32a0b84d90ed4cba2327dbba67212
aly3n <aly3n.dev@gmail.com>
2022-06-02 21:34:32 +0430

    feat(web3): add connect wallet to navbar

---
+++ src/components/layout/Header/ConnectWallet.tsx
@@ -0,0 +1,13 @@
+const ConnectWallet:React.FC = () => {
+
+
+    return(
+        <button className="w-[8.125rem] h-10 z-50 text-sm text-black font-bold cursor-pointer pointer-events-auto flex justify-center items-center text-center
+        absolute top-[2rem] right-[4.5rem] bg-primary-500 hover:bg-primary-700 rounded-full">
+            Connect Wallet
+        </button>
+    );
+}
+
+
+export default ConnectWallet;
