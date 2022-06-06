fd41226046dae72ee830a2f11fdc669ef4ede3fa
aly3n <aly3n.dev@gmail.com>
2022-06-06 19:01:54 +0430

    feat(web3): add style and transition for profile btns

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -9,15 +9,15 @@ const Profile: React.FC = () => {
     <div className=" w-full min-h-screen flex flex-col gap-5 justify-center items-center text-3xl text-white">
       <h1>user Profile</h1>
       <button
-        className="h-16 bg-yellow-700 text-white px-5 rounded-full hover:bg-yellow-900"
+        className="h-16 bg-yellow-700 text-white px-5 rounded-full hover:bg-yellow-900 duration-300"
         onClick={disconnect}
       >
         Disconnect
       </button>
-      <button onClick={signMessage} className="h-16 bg-red-700 text-white px-5 rounded-full hover:bg-red-900">
+      <button onClick={signMessage} className="h-16 bg-red-700 text-white px-10 rounded-full hover:bg-red-900 duration-300">
         verify
       </button>
-      <Link href="/profile/verify">To Private Path</Link>
+      <Link href="/profile/verify"><a className="h-16 px-10 flex justify-center items-center rounded-full bg-blue-700 hover:bg-blue-900 duration-300">To Private Path</a></Link>
     </div>
   );
 };
