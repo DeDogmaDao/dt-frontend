28bb2eedd3ece5810ed55ee47a05cb5ba7d1fa55
aly3n <aly3n.dev@gmail.com>
2022-06-08 00:17:26 +0430

    column design for benefit

--- src/components/howItWorksPage/benefits/EachBenefit.tsx
+++ src/components/howItWorksPage/benefits/EachBenefit.tsx
@@ -1,7 +1,5 @@
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { Dispatch, SetStateAction } from "react";
-import { benefitImg } from "../../../store/img";
 import { benefitType } from "../../../types/allTypes";
 import { eachBenefitAni } from "../../../utils/animation";
 interface props {
@@ -38,41 +36,18 @@ const EachBenefit: React.FC<props> = ({
   return (
     <motion.button
       onTap={() => setIsPlaying(false)}
-      className={`h-16 ml-8 sm:w-[11.25rem] sm:h-[6.5rem] md:w-[13.125rem] md:h-[7.5625rem]  lg:w-[17.5rem] lg:h-[10.125rem] xl:w-[21.5625rem] xl:h-[12.5rem] flex flex-col justify-center items-start sm:items-center
-       relative duration-75 sm:duration-500 ssm:ml-0 font-normal sm:backdrop-blur-[5.625rem] 
-        will-change-transform sm:rounded-[.9375rem] xl:rounded-[1.25rem]
-        overflow-hidden  ${
-         activeIndex === index
-           ? " text-white !font-bold sm:-translate-y-3 sm:scale-[1.12]"
-           : "text-neutral-50/30 "
-       }`}
+      className={`h-12 flex flex-col justify-center items-start relative duration-75 ml-6 ssm:ml-0 font-normal  ${
+        activeIndex === index ? " text-white !font-bold" : "text-neutral-50/30 "
+      }`}
       onClick={benefitBtnClickHandler}
     >
-      <motion.span className="sm:w-[11.25rem] sm:h-[6.5rem] md:w-[13.125rem] md:h-[7.5625rem] lg:w-[17.5rem] lg:h-[10.125rem] xl:w-[21.5625rem] xl:h-[12.5rem]  hidden sm:block will-change-transform">
-        <span className=" w-full h-full will-change-transform">
-          <Image
-            alt="dedogmadao logo"
-            src={benefitImg[index]}
-            layout={"responsive"}
-            quality={90}
-            placeholder="blur"
-            width="345"
-            height="200"
-          />
-        </span>
-      </motion.span>
-      <h3 className="sm:absolute sm:bottom-2 lg:bottom-5 sm:text-white
-      sm:font-bold text-lg sm:text-xs md:text-sm lg:text-lg will-change-transform"
-      style={{backfaceVisibility:"hidden", transform:"translateZ(0)"}}
-      >
-        {data.title}
-      </h3>
+      <h3>{data.title}</h3>
       {activeIndex === index && (
         <motion.div
           initial="hidden"
           animate="visible"
           variants={eachBenefitAni}
-          className="w-[1.625rem] h-[.375rem] bg-primary-500 absolute bottom-1 left-0 sm:hidden will-change-transform"
+          className="w-[1.625rem] h-[.375rem] bg-primary-500 absolute bottom-1 left-0"
         ></motion.div>
       )}
     </motion.button>
