d9e83963bc4f3e7ae04ca39b7c07eb246ba7a51a
aly3n <aly3n.dev@gmail.com>
2022-06-08 01:17:57 +0430

    change sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,5 +4,5 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://dedogmadao.com",
   changefreq: 'weekly',
   priority: 1,
-  exclude: ['/gameboard', '/gallery',"/underconstruction"],
+  exclude: ['/gameboard', '/gallery',"/underconstruction","/web3"],
 };
