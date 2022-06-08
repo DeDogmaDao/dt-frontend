8600a0fb5f4d97ed4366b5e8c4acb4e7aa4f6be4
aly3n <aly3n.dev@gmail.com>
2022-06-09 01:21:48 +0430

    add 404 & 500 error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,8 @@
+import { Fragment } from "react";
+import Error404 from "../components/errorPages/Error404";
+
+export default function Custom404() {
+    return <Fragment>
+        <Error404 />
+    </Fragment>
+  }
