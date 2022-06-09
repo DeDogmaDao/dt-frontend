b436100b8784cdd61d72801071fe14249916b47d
aly3n <aly3n.dev@gmail.com>
2022-06-10 00:41:14 +0430

    refactor:faq page

--- src/pages/auction.tsx
+++ src/pages/auction.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Fragment } from "react";
-import FAQ from "../components/faq/FAQ";
+import FAQ from "../components/faqPage";
 import { faqPageMetaData } from "../store/allData";
 
 export default function auction(): JSX.Element {
