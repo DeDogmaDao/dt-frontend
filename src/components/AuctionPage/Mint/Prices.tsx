347a0c089ab42fec77def0a7c28f1025640958d0
aly3n <aly3n.dev@gmail.com>
2022-06-16 16:40:12 +0430

    feat(auction): hardcode prices

--- src/components/AuctionPage/Mint/Prices.tsx
+++ src/components/AuctionPage/Mint/Prices.tsx
@@ -9,10 +9,10 @@ const Prices:React.FC<props> = ({auctionDropPerStep,endPrice,startPrice}) => {
 
     return(
         <div className="flex flex-col gap-y-4">
-            <div>Start Price: {startPrice} ETH</div>
-            <div>The amount of price drop: {auctionDropPerStep} ETH</div>
-            {/* <div>Resting Price: {prices.purchase} ETH</div> */}
-            <div>Resting Price: {endPrice} ETH</div>
+            <div>Start Price: 10 ETH</div>
+            <div>The amount of price drop: 0.075 ETH</div>
+            <div>Resting Price: 1 ETH</div>
+            <div>purchase Price: 2 ETH</div>
         </div>
     )
 }
