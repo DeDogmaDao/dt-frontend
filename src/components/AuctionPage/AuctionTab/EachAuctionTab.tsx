77f22e672852d4b8ca1882ee97fcd1e7d1fc2194
aly3n <aly3n.dev@gmail.com>
2022-06-16 16:48:09 +0430

    feat(auction): add each auction tab

---
+++ src/components/AuctionPage/AuctionTab/EachAuctionTab.tsx
@@ -0,0 +1,10 @@
+const EachAuctionTab:React.FC =() => {
+    return (
+        <div className="flex justify-center items-center w-[42.3125rem] h-[3.25rem]">
+            
+        </div>
+    )
+}
+
+
+export default EachAuctionTab;
--- src/components/AuctionPage/AuctionTab/index.tsx
+++ src/components/AuctionPage/AuctionTab/index.tsx
@@ -1,9 +1,17 @@
-const AuctionTab:React.FC = () => { 
-    return (
-        <div className="flex justify-center items-center w-[42.3125rem] h-[3.25rem]">
-            
-        </div>
-    )
-}
+import EachAuctionTab from "./EachAuctionTab";
+
+const auctionData = ["Mint", "About", "Other GodHolders"];
+
+const AuctionTab: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center w-[42.3125rem] h-[3.25rem]">
+        {auctionData.map((auction, index) => {
+            return (
+                <EachAuctionTab />
+            )
+        })}
+    </div>
+  );
+};
 
 export default AuctionTab;
