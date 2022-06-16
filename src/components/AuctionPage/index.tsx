09d7a035fea69295b6521116ed02d9fe09d098d2
aly3n <aly3n.dev@gmail.com>
2022-06-16 18:06:14 +0430

    feat(auction): shape the auction section

--- src/components/AuctionPage/index.tsx
+++ src/components/AuctionPage/index.tsx
@@ -2,26 +2,32 @@ import Mint from "./Mint";
 import { auctionData } from "../../store/allData";
 import { useState } from "react";
 import AuctionTab from "./AuctionTab";
+import About from "./About";
+import OtherGodHolders from "./OtherGodHolders";
 const AuctionPage: React.FC = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [activeTab, setActiveTab] = useState(0);
   return (
-    <div className="w-full h-full flex justify-center items-center ">
-      <AuctionTab activeTab={activeTab} setActiveTab={setActiveTab} />
+    <div className="w-screen min-h-screen flex justify-center items-center px-20">
+      <div className="w-1/2 h-full flex flex-col justify-center items-start bg-red-200/20">
+        <AuctionTab activeTab={activeTab} setActiveTab={setActiveTab} />
 
-      
-      {auctionData.map((auction, index) => {
-        if (index !== activeIndex) {
-          return null;
-        }
-        return (
-          <>
-            {activeTab === 0 && <Mint index={index} />}
-            {activeTab === 1 && "<Mint index={index} />"}
-            {activeTab === 2 && "<Mint index={index} />"}
-          </>
-        );
-      })}
+        <div className="w-[42.3125rem] h-[20.9375rem] bg-green-400">
+          {auctionData.map((auction, index) => {
+            if (index !== activeIndex) {
+              return null;
+            }
+            return (
+              <>
+                {activeTab === 0 && <Mint index={index} />}
+                {activeTab === 1 && <About />}
+                {activeTab === 2 && <OtherGodHolders />}
+              </>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-1/2 h-full flex flex-col justify-center items-center bg-red-100"></div>
     </div>
   );
 };
