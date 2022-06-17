1125fe62dcd15744b7bbb1f2673dfbeb7f19712e
aly3n <aly3n.dev@gmail.com>
2022-06-18 00:39:05 +0430

    feat(auction): add timer component

---
+++ src/components/global/Timer/index.tsx
@@ -0,0 +1,11 @@
+const Timer:React.FC = () => {
+
+    return(
+        <div className="w-52 h-11 flex justify-between items-center overflow-hidden">
+            <div></div>
+        </div>
+    )
+}
+
+
+export default Timer;
