94d347ae5dabb5b0bbd4b004a93e53ceb32948b3
aly3n <aly3n.dev@gmail.com>
2022-06-20 01:48:28 +0430

    feat(auction): fix

--- src/components/AuctionPage/AuctionTab/EachAuctionTab.tsx
+++ src/components/AuctionPage/AuctionTab/EachAuctionTab.tsx
@@ -18,11 +18,11 @@ const EachAuctionTab: React.FC<props> = ({
     <button
       onClick={() => setActiveTab(index)}
       className={`flex justify-center items-center w-max h-full rounded-t-lg px-6 py-3 gap-x-2 duration-300 hover:bg-[#191C3A5C]/60 ${
-        activeTab === index && "bg-[#191C3A5C]/30"
+        activeTab === index && "bg-[#191C3A5C]/30 text-primary-500"
       }`}
     >
       <span className=" stroke-primary-500 w-6 h-6">{tabIcon}</span>
-      <p>{tabName}</p>
+      <p className="text-xl font-semibold">{tabName}</p>
     </button>
   );
 };
