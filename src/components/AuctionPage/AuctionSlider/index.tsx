be9a9ebf5b7970e72355f269e3c3d78a72dbc1e8
aly3n <aly3n.dev@gmail.com>
2022-06-20 04:22:18 +0430

    feat(auction): add auction slider component to auction

---
+++ src/components/AuctionPage/AuctionSlider/index.tsx
@@ -0,0 +1,11 @@
+const AuctionSlider:React.FC = () =>{
+
+    return(
+        <div className="w-[33.75rem] h-[40rem] flex flex-col justify-center items-center">
+            <div className="w-[22.5rem] h-[38.125rem]"></div>
+        </div>
+    )
+}
+
+
+export default AuctionSlider;
