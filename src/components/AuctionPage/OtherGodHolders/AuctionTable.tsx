cc60e6811644faf7b3e53ce72c5af5dfc1dac817
aly3n <aly3n.dev@gmail.com>
2022-06-20 04:14:15 +0430

    feat(auction): fix

--- src/components/AuctionPage/OtherGodHolders/AuctionTable.tsx
+++ src/components/AuctionPage/OtherGodHolders/AuctionTable.tsx
@@ -1,10 +1,11 @@
+import { AngleRightSVG } from "../../../store/svg";
 import { auctionResultType, auctionDataType } from "../../../types/allTypes";
 
 interface props {
-    apiData: auctionResultType[] | [];
-    data:auctionDataType[];
+  apiData: auctionResultType[] | [];
+  data: auctionDataType[];
 }
-const AuctionTable: React.FC<props> = ({apiData,data}) => {
+const AuctionTable: React.FC<props> = ({ apiData, data }) => {
   return (
     <table className="table-auto w-full table border-collapse">
       <thead>
@@ -17,17 +18,31 @@ const AuctionTable: React.FC<props> = ({apiData,data}) => {
         </tr>
       </thead>
       <tbody>
-{data.map((item,index)=>{
-    return (
-        <tr className="h-16  text-left border-b last-of-type:border-0 border-neutral-500/50 hover:bg-primary-500/20 duration-300">
-        <td className="text-lg font-normal pl-4">day {index + 1}</td>
-        <td className="text-lg font-normal">{item.godName}</td>
-        <td className="text-lg font-normal">{apiData[index].purchasePrice}</td>
-        <td className="text-lg font-normal">{apiData[index].owner.substring(0,5) + "..." + apiData[index].owner.substring(apiData[index].owner.length-5)}</td>
-        <td className="font-medium text-base link-inside-text cursor-pointer">More info</td>
-      </tr>
-    )
-})}
+        {data.map((item, index) => {
+          return (
+            <tr className="h-16  text-left border-b last-of-type:border-0 border-neutral-500/50 hover:bg-primary-500/20 duration-300">
+              <td className="text-lg font-normal pl-4">day {index + 1}</td>
+              <td className="text-lg font-normal">{item.godName}</td>
+              <td className="text-lg font-normal">
+                {apiData[index].purchasePrice}
+              </td>
+              <td className="text-lg font-normal">
+                {apiData[index].owner.substring(0, 5) +
+                  "..." +
+                  apiData[index].owner.substring(
+                    apiData[index].owner.length - 5
+                  )}
+              </td>
+              <td className="group font-medium text-base text-primary-500 hover:text-secondary-500 duration-300
+               !no-underline cursor-pointer flex items-center justify-center h-16 gap-x-3">
+                <span>More info</span>
+                <span className="!w-2 h-4 flex justify-center items-center fill-transparent stroke-primary-500 group-hover:stroke-secondary-500 duration-300">
+                  <AngleRightSVG />
+                </span>
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
