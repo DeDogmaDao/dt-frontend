b1213eb1fc284658d8c7f1a1a3ef9f7f9b8d9839
aly3n <aly3n.dev@gmail.com>
2022-06-20 01:16:43 +0430

    feat(auction): add divider border to table

--- src/components/AuctionPage/OtherGodHolders/AuctionTable.tsx
+++ src/components/AuctionPage/OtherGodHolders/AuctionTable.tsx
@@ -6,7 +6,7 @@ interface props {
 }
 const AuctionTable: React.FC<props> = ({apiData,data}) => {
   return (
-    <table className="table-auto w-full table">
+    <table className="table-auto w-full table border-collapse">
       <thead>
         <tr className="h-11 text-white/60 text-left">
           <th className="text-base font-normal">N.</th>
@@ -19,7 +19,7 @@ const AuctionTable: React.FC<props> = ({apiData,data}) => {
       <tbody>
 {data.map((item,index)=>{
     return (
-        <tr className="h-16  text-left ">
+        <tr className="h-16  text-left border-b border-neutral-500/50">
         <td className="text-lg font-normal">day {index}</td>
         <td className="text-lg font-normal">{item.godName}</td>
         <td className="text-lg font-normal">{apiData[index].purchasePrice}</td>
