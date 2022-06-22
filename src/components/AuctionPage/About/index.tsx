3a67693aab942356d67ddfb29deee791a5406e62
aly3n <aly3n.dev@gmail.com>
2022-06-23 00:43:47 +0430

    feat(auction): gap for about section

--- src/components/AuctionPage/About/index.tsx
+++ src/components/AuctionPage/About/index.tsx
@@ -4,7 +4,7 @@ interface props {
   data:auctionDataType;
 }
 const About: React.FC<props> = ({data}) => {
-  return <div className="w-full h-full flex flex-col justify-start items-start px-6 py-8 text-xl font-light">
+  return <div className="w-full h-full flex flex-col justify-start items-start gap-y-2 px-6 py-8 text-xl font-light">
     {data.about.map(item=>{
       return <p>{item}</p>
     })}
