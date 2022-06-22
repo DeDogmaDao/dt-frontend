fc367aacbf33768ae95dc7cfd86759ac6ab9bdbb
aly3n <aly3n.dev@gmail.com>
2022-06-23 02:43:04 +0430

    feat(auction): some style for about section

--- src/components/AuctionPage/About/index.tsx
+++ src/components/AuctionPage/About/index.tsx
@@ -6,7 +6,7 @@ interface props {
 const About: React.FC<props> = ({data}) => {
   return <div className="w-full h-full flex flex-col justify-start items-start gap-y-2 px-6 py-8 text-xl font-light">
     {data.about.map(item=>{
-      return <p>{item}</p>
+      return <p className="first-letter:text-primary-50 first-letter:font-bold">{item}</p>
     })}
 
   </div>;
