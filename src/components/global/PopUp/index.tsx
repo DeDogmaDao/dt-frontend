6f6d65cb2e67580862d8d7b53a49dbc728ea1074
aly3n <aly3n.dev@gmail.com>
2022-06-28 20:29:57 +0430

    feat(auction): complete pop-up

--- src/components/global/PopUp/index.tsx
+++ src/components/global/PopUp/index.tsx
@@ -1,10 +1,23 @@
-const PopUp:React.FC = () => {
-
-    return (
-        <div>
-
-        </div>
-    )
+import { MouseEventHandler } from "react";
+import Modal from "../Modal";
+interface props {
+  isOpen: boolean;
+  id: string;
+  onBackdropClick: MouseEventHandler<HTMLDivElement> | undefined;
 }
+const PopUp: React.FC<props> = ({ id, isOpen, onBackdropClick, children }) => {
+  return (
+    <Modal id={id} isOpen={isOpen} onBackdropClick={onBackdropClick}>
+      <div className="relative w-full h-full flex justify-center items-center backdrop-blur-[.8125rem]">
+        <div
+          className="w-[30.8125rem] h-[30.1875rem] rounded-[1.25rem] bg-body-800
+         overflow-hidden ring-2 ring-primary-500/30 flex flex-col justify-start shadow-[0_58px_33px_-42px_rgba(44,237,255,0.29)]"
+        >
+            {children}
+        </div>
+      </div>
+    </Modal>
+  );
+};
 
 export default PopUp;
