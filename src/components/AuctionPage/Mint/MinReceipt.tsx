a7fa18f299adb3fc9bcecbc11b4a7422eaf538e8
aly3n <aly3n.dev@gmail.com>
2022-07-01 02:16:46 +0430

    feat(auction): add style to mint receipt

--- src/components/AuctionPage/Mint/MinReceipt.tsx
+++ src/components/AuctionPage/Mint/MinReceipt.tsx
@@ -37,23 +37,25 @@ const MinReceipt: React.FC<props> = ({ status }) => {
       onBackdropClick={(e) => modalBackdropClickHandler(e)}
       type={modalType}
     >
-      <div className="w-full h-full flex flex-col px-8 gap-y-6 pt-10">
-        <div className="w-full flex justify-between">
+      <div className="w-full h-full flex flex-col px-8 gap-y-6 pt-10 text-base font-normal text-white/60">
+        <div className="w-full flex justify-between text-base font-normal">
           <span>Card name</span>
-          <span>{"cardName"}</span>
+          <span className="text-white">{"cardName"}</span>
         </div>
         <div className="w-full flex justify-between">
           <span>Amount paid</span>
-          <span>{"6.687" + " ETH"}</span>
+          <span className="text-white">{"6.687" + " ETH"}</span>
         </div>
         <div className="w-full flex justify-between">
           <span>Transaction ID</span>
-          <span>{"jhkjh329874"}</span>
+          <span className="text-white">{"jhkjh329874"}</span>
         </div>
         <div className="w-full flex justify-between">
           <span>Transaction time</span>
-          <span>{"2022/4/1 15:46"}</span>
+          <span className="text-white">{"2022/4/1 15:46"}</span>
         </div>
+        <div className="w-full border-t-2 border-white/40 border-dashed" />
+        <p className="text-center text-sm"> See more details of the transaction in Profile</p>
       </div>
     </PopUp>
   );
--- src/components/global/PopUp/index.tsx
+++ src/components/global/PopUp/index.tsx
@@ -15,7 +15,7 @@ const PopUp: React.FC<props> = ({ id, isOpen, onBackdropClick, children,type }) 
     <Modal id={id} isOpen={isOpen} onBackdropClick={onBackdropClick}>
       <div className="relative w-full h-full flex justify-center items-center backdrop-blur-[.8125rem]">
         <div
-          className="w-[30.8125rem] h-[30.1875rem] rounded-[1.25rem] bg-body-800
+          className="w-[30.8125rem] pb-8 rounded-[1.25rem] bg-body-800
          overflow-hidden ring-2 ring-primary-500/30 flex flex-col justify-start shadow-[0_58px_33px_-42px_rgba(44,237,255,0.29)]"
         >
           <span className="w-full h-[6.3125rem] will-change-transform relative flex justify-center items-center">
