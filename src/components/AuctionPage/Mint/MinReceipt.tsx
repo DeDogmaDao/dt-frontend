5f2dc19dc257c8d01d444743354cc307aaf5c2dc
aly3n <aly3n.dev@gmail.com>
2022-07-01 02:03:37 +0430

    feat(auction):mint receipt body

--- src/components/AuctionPage/Mint/MinReceipt.tsx
+++ src/components/AuctionPage/Mint/MinReceipt.tsx
@@ -37,7 +37,24 @@ const MinReceipt: React.FC<props> = ({ status }) => {
       onBackdropClick={(e) => modalBackdropClickHandler(e)}
       type={modalType}
     >
-      <div></div>
+      <div className="w-full h-full flex flex-col px-8 gap-y-6 pt-10">
+        <div className="w-full flex justify-between">
+          <span>Card name</span>
+          <span>{"cardName"}</span>
+        </div>
+        <div className="w-full flex justify-between">
+          <span>Amount paid</span>
+          <span>{"6.687" + " ETH"}</span>
+        </div>
+        <div className="w-full flex justify-between">
+          <span>Transaction ID</span>
+          <span>{"jhkjh329874"}</span>
+        </div>
+        <div className="w-full flex justify-between">
+          <span>Transaction time</span>
+          <span>{"2022/4/1 15:46"}</span>
+        </div>
+      </div>
     </PopUp>
   );
 };
