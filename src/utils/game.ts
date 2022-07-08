aec6a3fe383860058f375f6598b91c7e5bf5f5aa
aly3n <aly3n.dev@gmail.com>
2022-07-08 19:42:02 +0430

    refactor(game): add useeffect to cardgame to avoid extra if checking

--- src/utils/game.ts
+++ src/utils/game.ts
@@ -36,3 +36,26 @@ export const topLeftSpell = {
   width: 0.2,
   height: 0.2,
 };
+
+
+export const communityNumStyles: React.CSSProperties = {
+  left: "0.3vw",
+  bottom: "0vw",
+};
+export const individualNumStyles: React.CSSProperties = {
+  right: "0.5vw",
+  bottom: "0vw",
+};
+export const styles = (column:number,cardIndex:number):React.CSSProperties=> ({
+  left: 6.55 + (column % 3) * 6.7 + "vw",
+  top: 4 + cardIndex / 50 + "vw",
+});
+export const spellStyles = (spellIndex: number) => {
+  const column = (spellIndex % 3) + 1;
+  return {
+    right: 5 - column * 0.5 + "vw",
+    bottom: -5 + Math.floor(spellIndex / 3) * 0.5 + "vw",
+    width: 0.2 + "vw",
+    height: 0.2 + "vw",
+  };
+};
