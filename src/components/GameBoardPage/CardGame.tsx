fab27b889279a694e495f4b8b0536aa15787c8ef
aly3n <aly3n.dev@gmail.com>
2022-07-14 16:39:11 +0430

    feat(game): change game data

--- src/components/GameBoardPage/CardGame.tsx
+++ src/components/GameBoardPage/CardGame.tsx
@@ -146,7 +146,7 @@ useLayoutEffect(()=>{
         <motion.img
           style={{ translateZ: "-1px" }}
           className="w-full object-contain absolute top-0 left-0"
-          src={"/img/team/member3.png"}
+          src={"/img/game/cardBack.png"}
         />
         <motion.img className="w-full object-contain" src={data.image} />
         {stage === 1 && (
