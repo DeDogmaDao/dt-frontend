9858f5bfc8769b9dd7697adb70ee7a346df87cda
aly3n <aly3n.dev@gmail.com>
2022-07-18 17:14:45 +0430

    fix(header): fix

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -38,11 +38,11 @@ const Header: React.FC = () => {
     <div className="absolute top-0 left-0 w-full h-[5.25rem] z-[10000] pointer-events-none">
       <div className="flex justify-start items-center w-full h-full relative">
         <div className="absolute top-0 left-0 w-full h-[7.5rem] bg-gradient-to-b from-[#090919] via-[#090919]/50 to-transparent " />
-        <span className="absolute top-[2.3rem]  left-5 ssm:left-16 z-100 pointer-events-auto cursor-pointer">
+        <span className="absolute top-[2.3rem]  left-5 md:left-16 z-100 pointer-events-auto cursor-pointer">
           <Link href="/">
             <div className="flex justify-start items-center gap-x-2 fill-white hover:fill-primary-500 duration-300">
               <LogoSVG width="2.5rem" />
-              <span className="text-small-bold ssm:text-medium-bold hidden sm:block md:hidden ">
+              <span className="text-small-bold ssm:text-medium-bold hidden ssm:block md:hidden ">
                 Demmortal Treasure
               </span>
             </div>
@@ -56,23 +56,23 @@ const Header: React.FC = () => {
         <a
           href={gitbookLink}
           target="_blank"
-          className="z-50 pointer-events-auto absolute top-[2rem] right-[7rem] px-2 ssm:w-[8.5rem] h-10 rounded-full bg-neutral-600 text-white text-sm font-bold 
-          justify-evenly items-center hover:bg-neutral-500 duration-200 fill-primary-500 hover:fill-primary-700 cursor-pointer "
+          className="z-50 pointer-events-auto absolute top-[2rem] right-[7.5rem] px-2 sm:w-[8.5rem] h-10 rounded-full bg-neutral-600 text-white text-sm font-bold 
+         flex justify-evenly items-center hover:bg-neutral-500 duration-200 fill-primary-500 hover:fill-primary-700 cursor-pointer "
         >
           <span className="">
             <socialSVGs.GitBookSVG width={26} />
           </span>
-          <span>Litepaper</span>
+          <span className="hidden sm:block">Litepaper</span>
         </a>
         <a
           href={discordLink}
           target="_blank"
-          className="z-50 absolute cursor-pointer pointer-events-auto top-[2rem] right-[13rem] w-10 h-10 rounded-full bg-neutral-600
+          className="z-50 absolute cursor-pointer pointer-events-auto top-[2rem] right-[4.5rem] w-10 h-10 rounded-full bg-neutral-600
           text-white text-xl flex justify-center items-center hover:bg-neutral-500 duration-200 fill-primary-500 hover:fill-primary-700"
         >
           <socialSVGs.DiscordSVG width={26} />
         </a>
-        <ConnectWallet />
+        {/* <ConnectWallet /> */}
         <MobileMenu headerLinks={headerLinks} />
       </div>
     </div>
