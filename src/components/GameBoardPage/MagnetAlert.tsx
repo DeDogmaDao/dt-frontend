b083772b7a3b794a688b0c158bccfccb8884df10
aly3n <aly3n.dev@gmail.com>
2022-08-10 22:15:51 +0430

    feat(game): add magnetAlert

---
+++ src/components/GameBoardPage/MagnetAlert.tsx
@@ -0,0 +1,10 @@
+const MagnetAlert:React.FC = () => {
+
+    return (
+        <div className="absolute top-[10vw] left-[25vw] h-[1vw] w-[5vw] text-[2vw] bg-red-500">
+
+        </div>
+    )
+}
+
+export default MagnetAlert;
