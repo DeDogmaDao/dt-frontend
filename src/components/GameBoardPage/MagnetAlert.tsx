b9abf518fa614aab993834f4b7e9df13bfcad4a7
aly3n <aly3n.dev@gmail.com>
2022-08-11 11:11:53 +0430

    feat(game): add glow text animation

--- src/components/GameBoardPage/MagnetAlert.tsx
+++ src/components/GameBoardPage/MagnetAlert.tsx
@@ -1,11 +1,12 @@
-const MagnetAlert:React.FC = () => {
-
-    return (
-        <div className="absolute top-[8vw] left-[30vw] h-[3vw] w-[7vw] text-[1.5vw] font-bold text-red-500 
-        rounded-sm ring-red-500 ring-4 ring-inset flex justify-center items-center">
-            <p>Magnet</p>
-        </div>
-    )
-}
+const MagnetAlert: React.FC = () => {
+  return (
+    <div
+      className="absolute top-[8vw] left-[30vw] h-[3vw] w-[7vw] text-[1.5vw] font-bold text-red-500 
+        rounded-sm ring-red-500 ring-4 ring-inset flex justify-center items-center "
+    >
+      <p className="animate-glowText">Magnet</p>
+    </div>
+  );
+};
 
 export default MagnetAlert;
