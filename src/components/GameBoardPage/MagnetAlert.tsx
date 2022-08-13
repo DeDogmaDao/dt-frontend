36e326784f6e14c337cc20ee5cd667778506fe69
aly3n <aly3n.dev@gmail.com>
2022-08-13 11:43:00 +0430

    feat(game): avoid select magnet alert

--- src/components/GameBoardPage/MagnetAlert.tsx
+++ src/components/GameBoardPage/MagnetAlert.tsx
@@ -16,12 +16,12 @@ const MagnetAlert: React.FC<props> = ({ currentCard }) => {
   }, [currentCard]);
   return (
     <div
-      className={`absolute top-[8vw] left-[30vw] h-[3vw] w-[7vw] text-[1.5vw] font-bold duration-1000
+      className={`absolute top-[8vw] left-[30vw] h-[3vw] w-[7vw] text-[1.5vw] font-bold duration-1000 select-none cursor-default
       rounded-sm  ring-4 ring-inset flex justify-center items-center ring-neutral-600 text-neutral-600 ${
         on > 0 && "!ring-red-500 !text-red-500 "
       }`}
     >
-      <p className={`duration-1000 ${on > 2 && "animate-glowText"}`}>Magnet</p>
+      <p className={`duration-1000 select-none ${on > 2 && "animate-glowText"}`}>Magnet</p>
     </div>
   );
 };
