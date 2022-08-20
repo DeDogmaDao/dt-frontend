ca874a8e012655fd3b56b91b682eb2b4505d28b1
aly3n <aly3n.dev@gmail.com>
2022-08-20 23:02:44 +0430

    refactor(updat): change clientOnly children type

--- src/hoc/ClientOnly.tsx
+++ src/hoc/ClientOnly.tsx
@@ -1,6 +1,9 @@
 import { useHasMounted } from "../hooks/useHasMounted";
+interface props {
+  children:React.ReactNode;
+}
 
-const ClientOnly: React.FC = ({ children }) => {
+const ClientOnly: React.FC<props> = ({ children }) => {
   const hasMounted = useHasMounted();
   if (!hasMounted) {
     return null;
