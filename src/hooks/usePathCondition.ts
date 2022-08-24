87b6322ba347a4e5237a513d26e5c5fd3c58bdf7
aly3n <aly3n.dev@gmail.com>
2022-08-24 22:52:42 +0430

    feat(hook): add usePathCondition

---
+++ src/hooks/usePathCondition.ts
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { useRef } from "react";
+
+export const usePathCondition = (url: string | string[]): boolean => {
+  const router = useRouter();
+  const result = useRef(false);
+
+  if (url[0]) {
+    if (typeof url === "string") {
+      if (router.asPath === url) {
+        result.current = true;
+      }
+    } else {
+      url.forEach((eachUrl) => {
+        if (router.asPath === eachUrl) {
+          result.current = true;
+        }
+      });
+    }
+  }
+  return result.current;
+};
