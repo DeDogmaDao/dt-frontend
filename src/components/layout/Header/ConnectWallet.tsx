54d5f25214d92a86dee3372f44f0cac34a36dc2e
dev-aly3n <aly3n.dev@gmail.com>
2024-03-28 01:02:18 +0330

    fix

--- src/components/layout/Header/ConnectWallet.tsx
+++ src/components/layout/Header/ConnectWallet.tsx
@@ -33,7 +33,7 @@ const ConnectWallet: React.FC = () => {
         onHoverStart={() => setIsHovered(true)}
         onHoverEnd={() => setIsHovered(false)}
         onClick={walletModalClickHandler}
-        className="w-[8.125rem] h-10 z-50 text-sm text-black font-bold cursor-pointer pointer-events-auto flex justify-center items-center text-center
+        className="w-[8.125rem] h-10 z-50 text-sm text-white font-bold cursor-pointer pointer-events-auto flex justify-center items-center text-center
         absolute top-[2rem] right-[4.5rem] bg-primary-500 hover:bg-primary-700 rounded-full"
       >
         <div className="relative w-full h-full flex justify-center items-center">
